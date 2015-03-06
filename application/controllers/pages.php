532033a829c2ad7e803dd3e22530de1bbd16e3bf
Iain White <iain@emu.com.au>
2015-03-06 10:34:04 +1000

    Full view paths

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -26,37 +26,17 @@ class Pages extends CI_Controller {
             return;
         }
         $section = NULL;
-        $level1 = NULL;
-        $level2 = NULL;
         $view = NULL;
         $segmentCount = count(($this->uri->segments));
-        if ($segmentCount > 3) {
+        $view = $this->uri->segment($segmentCount);
+        // Check if view has an alias e.g. 'microsoft-windows', 'ms-windows' returns 'windows'
+        //$view = checkAlias($view);
+        if ($segmentCount > 1) {
             $section = $this->uri->segment(1);
-            $level1 = $this->uri->segment(2);
-            $level2 = $this->uri->segment(3);
-            $view = $this->uri->segment(4);
-        }
-        if ($segmentCount == 3) {
-            $section = $this->uri->segment(1);
-            $level1 = $this->uri->segment(2);
-            $view = $this->uri->segment(3);
-        }
-        if ($segmentCount == 2) {
-            $section = $this->uri->segment(1);
-            $view = $this->uri->segment(2);
-        }
-        if ($segmentCount == 1) {
-            $view = $this->uri->segment(1);
         }
         $viewPath = '';
-        if ($section) {
-            $viewPath = $section . '/';
-        }
-        if ($level1) {
-            $viewPath = $viewPath . $level1 . '/';
-        }
-        if ($level2) {
-            $viewPath = $viewPath . $level2 . '/';
+        for ($x = 1; $x < $segmentCount; $x++) {
+            $viewPath .= $this->uri->segment($x) . '/';
         }
         $viewPath = $viewPath . $view;
         if (file_exists(APPPATH . 'views/pages/' . $viewPath . EXT)) {
@@ -72,28 +52,6 @@ class Pages extends CI_Controller {
             $this->stencil->paint('404_view');
         }
 
-        /*
-		switch ($this->uri->segment(1)) 
-		{
-			// This is used for quick static pages without having to deal with routing (see routes.php and the docs for more info)
-			// Just make the "case" look exactly like the URL you want
-			case 'terms-of-service' :
-				$this->stencil->title('License');
-				$data['subpage_text'] = 'MIT License';
-				$this->stencil->data($data);
-				$this->stencil->paint('license_view');
-				break;
-			
-			default :
-				$this->output->set_status_header('404');
-				
-				$this->stencil->title('404 Page Not Found');
-				$data['subpage_text'] = '404 Page Does not Exist!';
-				$this->stencil->data($data);
-				$this->stencil->paint('404_view');
-				break;
-		}
-        */
 	}
 }
 
