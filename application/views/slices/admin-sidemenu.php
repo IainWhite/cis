7af274c20d54ad770965940362de88409fc478f5
Iain White <iain@emu.com.au>
2015-03-17 16:43:22 +1000

    Basic side menus

---
+++ application/views/slices/admin-sidemenu.php
@@ -0,0 +1,8 @@
+<ul class="list-group sidebar-nav-v1 fa-fixed" id="sidebar-nav">
+    <li class="list-group-item list-toggle active">
+        <a data-toggle="collapse" data-parent="#sidebar-nav" href="#collapse-1">Stuff</a>
+        <ul id="collapse-1" class="collapse in">
+            <li class="active"><a href="#"><i class="fa fa-exchange"></i> Item 1</a></li>
+        </ul>
+    </li>
+</ul>
