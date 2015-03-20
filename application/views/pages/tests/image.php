6b34a3284724e76d91cdde822ae2638327bbaad6
Iain White <iain@emu.com.au>
2015-03-20 16:09:36 +1000

    image tests

---
+++ application/views/pages/tests/image.php
@@ -0,0 +1,107 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Image Tests'); ?>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h2>Left</h2>
+        <h3>Basic Image</h3>
+            <?php echo $this->stencil->addImage('Windows Vista Desktop', 'png', 'left', 'p'); ?>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h3>With title</h3>
+            <?php echo $this->stencil->addImage('Windows Vista Desktop', 'png', 'left', 'p', FALSE, NULL, TRUE); ?>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h3>With zoom and title</h3>
+            <?php echo $this->stencil->addImage('Windows Vista Desktop', 'png', 'left', 'p', TRUE, NULL, TRUE); ?>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h3>With zoom</h3>
+            <?php echo $this->stencil->addImage('Windows Vista Desktop', 'png', 'left', 'p', TRUE, NULL, FALSE); ?>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h4>Right</h4>
+        <h3>Basic Image</h3>
+            <?php echo $this->stencil->addImage('Windows Vista Desktop', 'png', 'right', 'p'); ?>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h3>With title</h3>
+            <?php echo $this->stencil->addImage('Windows Vista Desktop', 'png', 'right', 'p', FALSE, NULL, TRUE); ?>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h3>With zoom and title</h3>
+            <?php echo $this->stencil->addImage('Windows Vista Desktop', 'png', 'right', 'p', TRUE, NULL, TRUE); ?>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h3>With zoom</h3>
+            <?php echo $this->stencil->addImage('Windows Vista Desktop', 'png', 'right', 'p', TRUE, NULL, FALSE); ?>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h2>Centered</h2>
+        <h3>Basic Image</h3>
+    </div>
+</div>
+<div class="row">
+    <?php echo $this->stencil->addImage('Windows Vista Desktop', 'png', 'centre'); ?>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h3>With title</h3>
+    </div>
+</div>
+<div class="row">
+    <?php echo $this->stencil->addImage('Windows Vista Desktop', 'png', 'centre', 'p', FALSE, NULL, TRUE); ?>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h3>With zoom and title</h3>
+    </div>
+</div>
+<div class="row">
+    <?php echo $this->stencil->addImage('Windows Vista Desktop', 'png', 'centre', 'p', TRUE, NULL, TRUE); ?>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h3>With zoom</h3>
+    </div>
+</div>
+<div class="row">
+    <?php echo $this->stencil->addImage('Windows Vista Desktop', 'png', 'centre', 'p', TRUE, NULL, False); ?>
+</div>
