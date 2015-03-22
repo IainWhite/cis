8027ad37ccbbd31c84c6312f5d6bb178f43b8f82
Iain White <iain@emu.com.au>
2015-03-23 08:43:15 +1000

    Tests

--- application/views/pages/project-management/agile/scrum/sa.php
+++ application/views/pages/project-management/agile/scrum/sa.php
@@ -3,10 +3,8 @@
 <p>Another Bla Bla Bla</p>
 
 <?php
-//$this->stencil->title('SA Page');
 echo $this->stencil->setHeadline('SA Page');
 $this->stencil->meta(array('keywords' => 'A, B, C'));
-echo $this->stencil->dumpStencil();
 
 if (!$this->stencil->getIsSub()) {
     echo '<p>Not a Sub</p>';
