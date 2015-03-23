80dfca1eee31ed3688a5287c1adafddc0b64461b
Iain White <iain@whiteinternet.com>
2015-03-23 20:59:23 +1000

    Tables

--- application/views/pages/project-management/agile/scrum/sa.php
+++ application/views/pages/project-management/agile/scrum/sa.php
@@ -1,15 +1,14 @@
-<h3>SA PAGE</h3>
-
-<p>Another Bla Bla Bla</p>
-
-<?php
-echo $this->stencil->setHeadline('SA Page');
-$this->stencil->meta(array('keywords' => 'A, B, C'));
-
-if (!$this->stencil->getIsSub()) {
-    echo '<p>Not a Sub</p>';
-}
-?>
-
-<hr>
-[END PAGE]
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('SA Page'); ?>
+        <p>Another Bla Bla Bla</p>
+        <?php
+        $this->stencil->meta(array('keywords' => 'A, B, C'));
+        if (!$this->stencil->getIsSub()) {
+            echo '<p>Not a Sub</p>';
+        }
+        ?>
+        <hr>
+        <p>[END PAGE]</p>
+    </div>
+</div>
