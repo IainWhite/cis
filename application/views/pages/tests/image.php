a60a56ba3245c86d0a7f7f81b7d5563ef6e70368
Iain White <iain@emu.com.au>
2015-03-24 13:43:32 +1000

    More image tests

--- application/views/pages/tests/image.php
+++ application/views/pages/tests/image.php
@@ -4,7 +4,7 @@
     </div>
 </div>
 
-<!-- headshots -->
+<!-- Headshots -->
 <div class="row">
     <div class="col-lg-12 col-md-12">
         <h2>Headshot</h2>
@@ -109,6 +109,60 @@
      </div>
 </div>
 
+<!-- Logos -->
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h2>Logos</h2>
+        <h2>Left</h2>
+        <h3>Basic Image</h3>
+        <?php echo $this->stencil->addLogo('CentOs', 'png', 'left'); ?>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h3>With title</h3>
+        <?php echo $this->stencil->addLogo('CentOs', 'png', 'left', NULL, TRUE); ?>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h2>Right</h2>
+        <h3>Basic Image</h3>
+        <?php echo $this->stencil->addLogo('CentOs', 'png', 'right'); ?>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h3>With title</h3>
+        <?php echo $this->stencil->addLogo('CentOs', 'png', 'right', NULL, TRUE); ?>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh nulla, malesuada id dui at, fermentum eleifend sem. Vivamus sit amet rhoncus turpis. Curabitur congue enim a quam porttitor faucibus. Pellentesque at metus nulla. Vivamus vel ultrices augue, sed maximus arcu. Sed et aliquet diam. Maecenas ac urna ut metus volutpat accumsan at quis neque. Fusce pretium mi nec orci interdum pharetra. Vivamus ornare mollis nisl id condimentum. Maecenas finibus tincidunt urna, non vestibulum urna congue id.</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h2>Center</h2>
+        <h3>Basic Image</h3>
+        <?php echo $this->stencil->addLogo('CentOs', 'png', 'center'); ?>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h3>With title</h3>
+        <?php echo $this->stencil->addLogo('CentOs', 'png', 'center', NULL, TRUE); ?>
+    </div>
+</div>
 
 <!-- Images -->
 <div class="row">
