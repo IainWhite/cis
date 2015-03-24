83e917325cf60dd6565c55e943ab3867e0037364
Iain White <iain@emu.com.au>
2015-03-24 13:34:00 +1000

    Emu Design

---
+++ application/views/pages/iain-white/companies/emu-design.php
@@ -0,0 +1,22 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Emu Design'); ?>
+        <?php echo $this->stencil->addLogo('Emu Design', 'gif', 'left' , NULL, TRUE); ?>
+        <p>Emu Design (<?php echo $this->stencil->addExternalLink('www.emu.com.au', 'http://www.emu.com.au/'); ?>) is a leading full service multimedia and digital agency in Brisbane.</p>
+        <p>Emu Design provides services to well know clients including Amart Sports, EB Games, SuperCheap Auto, BCF, Signet, Smart Send, Rebel, The University of Queensland and Angel Flight.</p>
+        <p>Contract <?php echo $this->stencil->elapsedYears('08/12/2014', FALSE, TRUE, '01/04/2015') ?></p>
+        <p>While working at Emu Design I worked on many projects from green field sites to maintenance and support.</p>
+        <p>The majority of my work was using PHP (Drupal and Wordpress) and Cold Fusion, but I also used HTML5, CSS-3, JavaScript, jQuery, MySQL, MS SQLServer, Adobe Flex and ASP.Net.</p>
+
+        <hr>
+        <h2>Notes</h2>
+        Emu Design is a leading full service multimedia and digital agency in Brisbane.
+        Emu Design provides services to well know clients including Amart Sports, EB Games, SuperCheap Auto, BCF, Signet, Smart Send, Rebel, The University of Queensland and Angel Flight.
+        Website: http://www.emu.com.au/
+        Four month contract 08/12/2014 - 01/04/2015 as a Web Developer.
+        While working at Emu Design I worked on many projects from green field sites to maintenance and support.
+        The majority of my work was using PHP (Drupal and Wordpress) and Cold Fusion, but I also used HTML5, CSS-3, JavaScript, jQuery, MySQL, MS SQLServer, Adobe Flex and ASP.Net.
+
+
+    </div>
+</div>
