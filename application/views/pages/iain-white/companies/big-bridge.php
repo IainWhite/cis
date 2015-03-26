48780f0fbe76c1a2e1b710e09ad4041beab0e8e0
Iain White <iain@emu.com.au>
2015-03-27 09:51:18 +1000

    Big Bridge

--- application/views/pages/iain-white/companies/big-bridge.php
+++ application/views/pages/iain-white/companies/big-bridge.php
@@ -2,5 +2,34 @@
     <div class="col-lg-12 col-md-12">
         <?php echo $this->stencil->setHeadline('Big Bridge'); ?>
 
+        <p><strong>Address:</strong> 83 Main Street, Kangaroo Point, Queensland 4169 Australia</p>
+        <p><strong>Big Bridge</strong> was a leading full service production agency. That specialised in Online Video, Website Development, Digital Marketing and SEO.</p>
+        <p>Founded in 1992, Big Bridge worked with Governments at all levels, leading private sector companies, tertiary institutions and museums. The company’s experience extended from health, education and training through to major infrastructure construction.</p>
+
+        <p>Big Bridge is now part of <?php echo $this->stencil->addExternalLink('Speedwell', 'http://speedwell.com.au/'); ?>.</p>
+
+        <h2>Iain at Big Bridge</h2>
+        <p><strong>Role:</strong> <strong>Senior Web Developer</strong></p>
+        <p><strong>Dates:</strong> 10/2011 - 12/2011 (<?php echo $this->stencil->elapsedYears('01/10/2011', True, TRUE, '30/12/2011'); ?>)</p>
+        <p><strong>Skills:</strong> Drupal PHP mySQL HTML5 CSS-3 JavaScript jQuery IIS</p>
+
+        <p>During my short time at Big Bridge I developed one site from scratch in Drupal 7 as well as completing several partly completed Drupal 7 and HTML sites.</p>
+
+        <p>I was also involved in providing support and enhancements to sites developed in Drupal 5, 6 and 7.</p>
+
+        <h3>Clients</h3>
+        <ul>
+            <li>Battery World</li>
+            <li>Poolwerx</li>
+            <li>JKTech</li>
+            <li>PanAust</li>
+            <li>Keller</li>
+            <li>Office of the Information Commissioner</li>
+            <li>Veterans Rugby QLD</li>
+            <li>Electrical Trades Union</li>
+            <li>The RPS Group</li>
+            <li>Devine Woodvale</li>
+            <li>iConcierge</li>
+        </ul>
     </div>
 </div>
