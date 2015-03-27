01739ae8736755e237d7acee2067041e9d4843f7
Iain White <iain@emu.com.au>
2015-03-27 16:26:15 +1000

    Home Page

--- application/views/pages/iain-white/companies.php
+++ application/views/pages/iain-white/companies.php
@@ -5,15 +5,15 @@
         <p>This page is a list of companies that Iain White has worked at.</p>
 
         <ul>
-            <li><?php echo $this->stencil->addLink('Emu Design', 'iain-white/companies'); ?>, Brisbane AU, 08/12/2014 - 01/04/2015</li>
-            <li><?php echo $this->stencil->addLink('OnTheHouse', 'iain-white/companies'); ?>, Brisbane AU,</li>
-            <li><?php echo $this->stencil->addLink('Freenet', 'iain-white/companies'); ?>, Springfield AU,</li>
-            <li><?php echo $this->stencil->addLink('Big Bridge', 'iain-white/companies'); ?>, Brisbane AU,</li>
-            <li><?php echo $this->stencil->addLink('The AOT Group', 'iain-white/companies', NULL, FALSE, 'aot'); ?>, Brisbane AU,</li>
-            <li><?php echo $this->stencil->addLink('JPC Infonet', 'iain-white/companies', NULL, FALSE, 'jpc'); ?>, Trowbridge UK,</li>
-            <li><?php echo $this->stencil->addLink('TigerRedi', 'iain-white/companies'); ?>, Bristol UK,</li>
-            <li><?php echo $this->stencil->addLink('Anglian Network Systems', 'iain-white/companies', NULL, FALSE, 'ans'); ?>, Bristol UK,</li>
-            <li><?php echo $this->stencil->addLink('Reuters', 'iain-white/companies'); ?>, ???, UK,</li>
+            <li><?php echo $this->stencil->addLink('Emu Design', 'iain-white/companies'); ?>, Brisbane AU, 2014 - 2015</li>
+            <li><?php echo $this->stencil->addLink('OnTheHouse', 'iain-white/companies'); ?>, Brisbane AU, 2013 - 2014</li>
+            <li><?php echo $this->stencil->addLink('Freenet', 'iain-white/companies'); ?>, Springfield AU, 2012 - 2012</li>
+            <li><?php echo $this->stencil->addLink('Big Bridge', 'iain-white/companies'); ?>, Brisbane AU, 2001 - 2011</li>
+            <li><?php echo $this->stencil->addLink('The AOT Group', 'iain-white/companies', NULL, FALSE, 'aot'); ?>, Brisbane AU, 2006 - 20011</li>
+            <li><?php echo $this->stencil->addLink('JPC Infonet', 'iain-white/companies', NULL, FALSE, 'jpc'); ?>, Trowbridge UK, 2002 - 2006</li>
+            <li><?php echo $this->stencil->addLink('TigerRedi', 'iain-white/companies'); ?>, Bristol UK, 200 - 2001</li>
+            <li><?php echo $this->stencil->addLink('Anglian Network Systems', 'iain-white/companies', NULL, FALSE, 'ans'); ?>, Bristol UK , 1999 - 2000</li>
+            <li><?php echo $this->stencil->addLink('Reuters', 'iain-white/companies'); ?>, ???, UK, 1997 - 1999</li>
         </ul>
 
     </div>
--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -4,3 +4,42 @@
         <p>Home Page</p>
     </div>
 </div>
+
+<div class="row">
+    <div class="col-md-4">
+        <div class="thumbnails thumbnail-style">
+            <div class="thumbnail-img">
+                <div class="overflow-hidden">
+                    <a href="/iain-white/php-developer">
+                        <img class="img-responsive" src="/assets/images/php-logos.gif" alt="Back-end" title="PHP Developer">
+                    </a>
+            </div>
+            <a class="btn-more hover-effect" href="/iain-white/php-developer">PHP Developer</a>
+        </div>
+    </div>
+</div>
+    <div class="col-md-4">
+        <div class="thumbnails thumbnail-style thumbnail-kenburn">
+            <div class="thumbnail-img">
+                <div class="overflow-hidden">
+                    <a href="/iain-white/javascript-guru">
+                        <img class="img-responsive" src="/assets/images/web-logos.gif" alt="Front-end" title="JavaScript Guru">
+                    </a>
+                </div>
+                <a class="btn-more hover-effect" href="/iain-white/javascript-guru">JavaScript Guru</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="thumbnails thumbnail-style thumbnail-kenburn">
+            <div class="thumbnail-img">
+                <div class="overflow-hidden">
+                    <a href="/iain-white/scrum-master">
+                        <img  class="img-responsive" src="/assets/images/scrum-circle.jpg" alt="Scrum" title="Scrum Master">
+                    </a>
+                </div>
+                <a class="btn-more hover-effect" href="/iain-white/scrum-master">Scrum Master</a>
+            </div>
+        </div>
+    </div>
+</div>
