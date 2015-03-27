033e9299cbcc82cd2096abb08a485c236e8889ff
Iain White <iain@emu.com.au>
2015-03-27 13:34:39 +1000

    Project Manager

---
+++ application/views/pages/iain-white/project-manager.php
@@ -0,0 +1,20 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Iain White Project Manager'); ?>
+
+        <p>I have successfully managed the scoping, planning, deployment and delivery of projects in time and to budget.</p>
+
+        <p>While my background and first love was in Software Engineering, I have found my niche managing projects and people in an Agile environment.</p>
+
+        <p>I am an accomplished and fluent communicator and a certified Scrum Master with strong investigation, problem-solving and decision-making skills, combined with a pragmatic approach and sound business acumen.</p>
+
+        <p>I have worked in both small and large companies and a variety of industries including manufacture, ISP, health, travel, news and marketing.</p>
+
+        <p>I have been a <strong>Certified Scrum Master</strong> since November 2013.</p>
+
+        <p>I was Scrum Master for <?php echo $this->stencil->elapsedYears('01/01/2013', TRUE, TRUE, '31/07/2014'); ?> at OnTheHouse.</p>
+
+        <p>In this role I remove impedances that would block the team’s productivity. I worked closely with Product Owners, Stakeholders, and Infrastructure. I facilitated the Scrum meetings including Sprint Planning, Sprint Review, Sprint Retrospective and Daily Stand-up.</p>
+
+    </div>
+</div>
