cf9e0a0133a287287c3e2433b1b6bbb27c4ff6dd
Iain White <iain@emu.com.au>
2015-03-31 13:11:06 +1000

    Press

--- application/views/pages/iain-white/press-and-reviews.php
+++ application/views/pages/iain-white/press-and-reviews.php
@@ -3,5 +3,36 @@
         <?php echo $this->stencil->setHeadline('Press and Reviews for Iain White'); ?>
         <?php $this->stencil->setDescription('Websites developed by Iain White have been featured in the national press.'); ?>
         <?php $this->stencil->meta(array('keywords' => 'Iain White, Brisbane, Australia, PHP Developer, Web Developer, JavaScript Guru, Scrum Master')); ?>
+
+        <p class="text-center"><strong>Websites that I have developed have been reviewed by leading magazines.</strong></p>
+
+        <h3>WHAT CAR? Magazine Web Site Special 2000</h3>
+        <img src="/assets/images/whatcar2000.gif" alt="What Car? Web Guide 2000" title="What Car? Web Guide 2000" class="pull-left margin-right-10 margin-bottom-10">
+        <p><strong>www.proton.co.uk</strong><br />
+            <i class="fa fa-quote-left press"></i> Proton's entrant is a bright looking, funky site that's well laid out and very, very easy to get around. On the home page, there are links and buttons to everything - from details of the latest range to finance plans - so you don't have to spend ages trawling around to find your chosen topics.  Proton doesn't offer more features than most rivals, but all your options on models are laid out on clear tables, allowing you to see exactly what you get for your money. The dealer locator is also extremely easy to use - just click on the map of the UK to see a list of dealers in your area, complete with addresses and telephone numbers. Which just goes to show what many web masters would do well to note: sometimes simple can be far more effective than extravagant.</p>
+        <p class="text-center"><strong>Verdict: </strong>
+            Easy to negotiate, good looking and comprehensive, it's one of the best manufacturer sites. <i class="fa fa-quote-right press"></i></p>
+        <p class="text-center"><strong><em>One of only two sites to get the coveted <img src="/assets/images/whatcar-tick.png" alt="What Car Recommended" title="What Car Recommended" width="79" height="33" class="whatcar" > that year.</em></strong></p>
+        <hr>
+
+        <h3>WEBSPACE Magazine January 2001</h3>
+        <img src="/assets/images/webspace-cover.gif" alt="Webspace Magazine" title="Webspace Magazine" class="pull-left margin-right-10 margin-bottom-10" >
+        <p><strong>www.proton.co.uk</strong><br />
+            <i class="fa fa-quote-left press"></i> One for the addicted. Proton.co.uk is the definitive car site, with links to motor sport and car dealers' sites, as well as finance and insurance. With plenty of pictures, clear signposting and detailed descriptions of how, what, when and where. Proton also advertises the company's current special offers. Easy and attractive to use the site is a real man puller. <i class="fa fa-quote-right press"></i></p>
+
+        <hr>
+
+        <h3>REVOLUTION Magazine</h3>
+        <img src="/assets//images/revolution.gif" alt="Revolution" title="Revolution Magazine" class="pull-left margin-right-10 margin-bottom-10" >
+        <p>Revolution magazine often followed our activities at <strong>TigerRedi</strong>, including our the Kia Cars UK web site in 2001 and our bid for the Skoda Cars UK web site also in 2001.</p>
+        <p><strong>Kia ties in relaunch with new car range</strong><br />
+            <em><small>30<sup>th</sup> May 2001 RICHARD JOLLEY</small></em><br /></p>
+        <p><i class="fa fa-quote-left press"></i> Motoring brand Kia Cars UK is tying in an overhaul of its site (www.kia.uk.com) with the launch of three new models. The firm, which is franchised in the UK to MCL Group from Hyundai-owned Kia Motors in Korea, will showcase the new Magentis, Mentor and Rio models on the site over the summer.</p>
+        <p>Both the Magentis and the Mentor will feature when the new site goes live at the beginning of July, and the Rio will be added later on.</p>
+        <p>Bristol-based agency TigerRedi has been given the job of overhauling the site.</p>
+        <p>Ben Saul, director of TigerRedi, explained that the site's final look was being discussed with Kia Cars' UK chiefs, but that it would have a new appearance and feel and feature the company's eight UK models. "The client wants to take the site forward quickly," he said.</p>
+        <p>Hannah Mullis, marketing assistant of Kia Cars UK, said the site would offer information on financing and the company's responsible motoring campaign encouraging drivers to take fewer short journeys.  "This time we have been given a bigger budget to produce the site," she said She added that the company was also keen to add an e-shop selling Kia-branded clothing.</p>
+        <p>TigerRedi's Saul said the site would continue to grow after its unveiling in July, with a dealer-locator service possibly being added later in the summer. <i class="fa fa-quote-right press"></i></p>
+
     </div>
 </div>
