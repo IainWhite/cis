c60f27972fde760fd7e69f66669e3616474ea7da
Iain White <iain@emu.com.au>
2015-03-31 13:26:38 +1000

    Recommendations

--- application/views/pages/iain-white/recommendations.php
+++ application/views/pages/iain-white/recommendations.php
@@ -3,5 +3,70 @@
         <?php echo $this->stencil->setHeadline('Recommendations for Iain White'); ?>
         <?php $this->stencil->setDescription('Recommendations for Iain White a Web Developer / Scrum Master based in Brisbane, Australia.'); ?>
         <?php $this->stencil->meta(array('keywords' => 'Iain White, Brisbane, Australia, PHP Developer, Web Developer, JavaScript Guru, Scrum Master, Resume, CV, Skills')); ?>
+        <p>Here are some recommendations about my work that people have been kind enough to give me.</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-6 col-md-6">
+        <div class="speechbubble rounded-2x">
+            Iain and I worked together on a number of successful projects.<br /><br />
+            Iain's experience in a number of varied industries and his ability to adapt to different technologies and developmental environments allows him to become a valuable and reliable resource very quickly.<br /><br />
+            Iain works equally well independently with little to no supervision or in larger project teams.<br /><br />
+            I would recommend Iain for any role he puts his name up for.
+        </div>
+        <p class="speechbubble-author">Tim Bland, Senior Systems Analyst, The AOT Group</p>
+    </div>
+    <div class="col-lg-6 col-md-6">
+        <div class="speechbubble rounded-2x">
+            I worked with Iain in the eCommerce team at the AOT Group for approximately 5 years. During this time Iain worked as the lead developer and system architect.<br /><br />
+            Iain's in depth knowledge of a large number of software languages and technical concepts allows him to be able to identify solutions and opportunities within very complex system environments.<br /><br />
+            Within Iain's leadership role he was also one of the key authors of the teams SDLC policies which have become the backbone of the teams workflow. <br /><br />
+            Iain is a pleasure to work with and would be an asset on any development team.
+        </div>
+        <p class="speechbubble-author">Anthoney Woodyard, General Manager - Distribution Systems, The AOT Group</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <div class="speechbubble rounded-2x">
+            It has not only been a pleasure working with you, but I can safely say that you would be an important asset to any business after a dependable, skilful and adoptable programmer, with managemnet expertise where required.
+        </div>
+        <p class="speechbubble-author">Darren Earny, Business Development and Operations Director, JPC Infonet</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-6 col-md-6">
+        <div class="speechbubble rounded-2x">
+            Whilst at TigerRedi we looked high and low for a developer that had real creative flair and imagination.<br /><br />
+            When Iain was recommend to us we jumped at the chance to bring him in to our team.<br /><br />
+            Iain works very hard and is a mind of technical knowledge!<br /><br />
+            Iain worked on accounts and build sites and applications for Honda, Kia Cars and Proton to name a few at TigerRedi with much enthusiasm.<br /><br />
+            I would'nt hesitate to recommend Iain to any potential recruitment opportunity, we all certainly enjoyed working with Iain.
+        </div>
+        <p class="speechbubble-author">Kevin Haynes, Director, TigerRedi</p>
+    </div>
+    <div class="col-lg-6 col-md-6">
+        <div class="speechbubble rounded-2x">
+            I have worked with many developers in the past but none have shown the ability as Iain has to build sites completely cross platform and to show honest creative thinking within a technical enviroment.<br /><br />
+            This in turn gave great confidence as a Creative Director to guarantee the right results for our clients.<br /><br />
+            Iain gave nothing but 100% in commitment and was a total team player never hesitating to help out in areas well outside of his role's remit.<br /><br />
+            He was the engine within the business, a valued and well liked member of the team and I would not hesitate to recommend him to a future employer.
+        </div>
+        <p class="speechbubble-author">Ben Saul, Creative Director, TigerRedi</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <div class="speechbubble rounded-2x">
+            Mr. White has been responsible for the development and implementation of our Airbus Computing Engineering System (ACES) which is a user friendly operating environment giving controlled, simple access to the various CAD / CAM applications and management of their data: PANOPLIE - Nesting System, ANVIL - BAe 3D CAD / CAM System, PPAS - Part Program Admin. System, Post Processing - Various NC post processors, Tape Punching - NC Tape Punching and Verification, TTPS - Tape Time Prediction System, File transfer - IBM to VAX.<br /><br />
+            Mr. White has also been responsible for the first implementation of Distributed Numerical Control (DNC) to a CNC Router. This is the foundation for a full DNC implementation to our Machining Facility.<br /><br />
+            Mr. White has a thorough knowledge of the engineering requirements which has led to a high standard of developed software now in full time production use.<br /><br />
+            Mr. White is punctual, his attendance is good, and he is a competent and respected member of Technical Systems Development.
+        </div>
+        <p class="speechbubble-author">Ray F. Levett, Manager, Technical Systems Development, Airbus Computing</p>
     </div>
 </div>
