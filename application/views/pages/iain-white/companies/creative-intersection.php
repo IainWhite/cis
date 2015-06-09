8b763f55a8028f8aeb79d1145369e0248a56badc
Iain White <iain@whiteinternet.com>
2015-06-09 17:43:39 +1000

    New pages

---
+++ application/views/pages/iain-white/companies/creative-intersection.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Creative Intersection'); ?>
+
+    </div>
+</div>
+
+<?php echo $this->stencil->addSiteImages('Creative Intersection'); ?>
