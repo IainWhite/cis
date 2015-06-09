80736a5c6bd83a0618f572cbcda7307d661d7357
Iain White <iain@whiteinternet.com>
2015-06-09 16:05:08 +1000

    Next pages

---
+++ application/views/pages/project-management/project-plan.php
@@ -0,0 +1,15 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Project Plan'); ?>
+        <p><strong>Project planning</strong> is a discipline for stating how to complete a project within a certain timeframe, usually with defined stages, and with designated resources. One view of project planning divides the activity into:</p>
+        <ul>
+            <li>Setting objectives (these should be measurable)</li>
+            <li>Identifying deliverables</li>
+            <li>Planning the schedule</li>
+            <li>Making supporting plans</li>
+        </ul>
+        <p>Supporting plans may include those related to: human resources, communication methods, and risk management.</p>
+        <p>Computer hardware and software project planning within an enterprise is often done using a project planning guide that describes the process that the enterprise feels has been successful in the past.</p>
+        <p>Tools popularly used for the scheduling part of a plan include the <?php echo $this->stencil->addLink('Gantt chart', '/project-management'); ?> and the <?php echo $this->stencil->addLink('PERT chart', '/project-management'); ?>.</p>
+    </div>
+</div>
