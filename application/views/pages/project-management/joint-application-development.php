5683414558f75a0517419157c22ac17ceff5a0b7
Iain White <iain@whiteinternet.com>
2015-06-09 17:06:39 +1000

    New pages

---
+++ application/views/pages/project-management/joint-application-development.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('JAD (Joint Application Development)'); ?>
+        <p><strong>JAD (Joint Application Development)</strong> is a methodology that involves the client or end user in the design and development of an application, through a succession of collaborative workshops called JAD sessions. <strong>Chuck Morris</strong> and Tony Crawford, both of <?php echo $this->stencil->addLink('IBM', '/companies'); ?>, developed JAD in the late 1970s and began teaching the approach through workshops in 1980.</p>
+        <p>The JAD approach, in comparison with the more traditional practice, is thought to lead to faster development times and greater client satisfaction, because the client is involved throughout the development process. In comparison, in the traditional approach to systems development, the developer investigates the system requirements and develops an application, with client input consisting of a series of interviews.</p>
+    </div>
+</div>
---
+++ application/views/pages/project-management/rapid-application-development.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('RAD (Rapid Application Development)'); ?>
+        <p><strong>RAD (Rapid Application Development)</stronog> is a concept that products can be developed faster and of higher quality through:</p>
+        <ul>
+            <li>Gathering requirements using workshops or focus groups</li>
+            <li>Prototyping and early, reiterative user testing of designs</li>
+            <li>The re-use of software components</li>
+            <li>A rigidly paced schedule that defers design improvements to the next product version</li>
+            <li>Less formality in reviews and other team communication</li>
+        </ul>
+        <p>Some companies offer products that provide some or all of the tools for RAD software development. (The concept can be applied to hardware development as well.) These products include requirements gathering tools, prototyping tools, computer-aided software engineering tools, language development environments such as those for the <?php echo $this->stencil->addLink('Java', '/languages'); ?> platform, groupware for communication among development members, and testing tools. RAD usually embraces object-oriented programming methodology, which inherently fosters software re-use. The most popular object-oriented programming languages, <?php echo $this->stencil->addLink('C++', '/languages'); ?> and Java, are offered in visual programming packages often described as providing Rapid Application Development.</p>
+    </div>
+</div>
