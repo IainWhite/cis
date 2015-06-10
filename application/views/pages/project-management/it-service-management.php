50e5e592c22c0b7e91aea5c21c6e1314cc520d35
Iain White <iain@whiteinternet.com>
2015-06-10 16:22:31 +1000

    New Pages

---
+++ application/views/pages/project-management/it-service-management.php
@@ -0,0 +1,22 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('IT Service Management (ITSM)'); ?>
+        <p><strong>IT Service Management (ITSM)</strong> refers to the implementation and management of quality information technology services. IT service management is performed by IT service providers through people, process and information technology.</p>
+        <p>The following represents a characteristic statement from the ITSM literature:</p>
+        <p><em>Providers of IT services can no longer afford to focus on technology and their internal organization they now have to consider the quality of the services they provide and focus on the relationship with customers.</em></p>
+        <p>ITSM is process-focused and has ties and common interests with process improvement frameworks and methodologies. The discipline is not concerned with the details of how to use a particular vendor's product, or necessarily with the technical details of the systems under management. Instead, it focuses upon providing a framework to structure IT-related activities and the interactions of IT technical personnel with business customers and users.</p>
+        <p>IT service management in the broader sense overlaps with the disciplines of business service management and IT portfolio management, especially in the area of IT planning and financial control.</p>
+        <p>ITSM is generally concerned with the "back office" or operational concerns of information technology management (sometimes known as operations architecture), and not with technology development. In this respect, ITSM may be seen as analogous to an <strong>Enterprise Resource Planning (ERP)</strong> discipline for IT.</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h3>Information Technology Infrastructure Library (ITIL)</h3>
+        <p>IT Service Management is often equated with the <strong>Information Technology Infrastructure Library</strong> (<strong>ITIL</strong>), an official publication of the Cabinet Office in the United Kingdom.</p>
+        <p>However, while a version of ITSM is a component of ITIL, ITIL also covers a number of related but distinct disciplines and the two are not synonymous.</p>
+        <p>The ownership of ITIL has transferred from the Office of Government Commerce (OGC) to the Cabinet Office, following the move of OGC into the Cabinet Office.</p>
+        <p>The current version of the <?php echo $this->stencil->addLink('ITIL', 'PM'); ?> framework is the 2011 edition. The 2011 edition, published in July 2011, is a revision of the previous edition known as ITIL version 3 (published in June 2007).It was a major upgrade from version 2 (2001). Whereas version 2 was process oriented (split into two groups: service support and service delivery), version 3 is service oriented.</p>
+        <p>Since ITIL V3, the various ITIL processes are grouped into five stages of the service lifecycle: service strategy, service design, service transition, service operation and Continual Service Improvement (or CSI).</p>
+        <p>The use of the term "service management" is interpreted by many in the world as ITSM, but again, there are other frameworks, and conversely, the entire ITIL library might be seen as IT service management in a larger sense.</p>
+    </div>
+</div>
---
+++ application/views/pages/project-management/it-service-managment/information-technology-infrastructure-library.php
@@ -0,0 +1,11 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('The Information Technology Infrastructure Library (ITIL)'); ?>
+        <p><strong>The Information Technology Infrastructure Library (ITIL)</strong> is a set of practices for <?php echo $this->stencil->addLink('IT service management', 'PM'); ?> (ITSM) that focuses on aligning IT services with the needs of business.</p>
+        <p>In its current form (known as ITIL 2011 edition), ITIL is published in a series of five core volumes, each of which covers an ITSM lifecycle stage.</p>
+        <p>ITIL underpins ISO/IEC 20000 (previously BS15000), the International Service Management Standard for IT service management, although differences between the two frameworks do exist.</p>
+        <p>ITIL describes processes, procedures, tasks and check lists that are not organization-specific, used by an organization for establishing integration with the organization's strategy, delivering value and maintaining a minimum level of competency.</p>
+        <p>It allows the organization to establish a baseline from which it can plan, implement and measure. It is used to demonstrate compliance and to measure improvement.</p>
+        <p>The acronym <strong>ITIL</strong> is a registered trademark of the United Kingdom's Cabinet Office. Following this move, the ownership is now listed as being with HM Government rather than the Office of Government Commerce (OGC). The publications continue to be Crown Copyright.</p>
+    </div>
+</div>
