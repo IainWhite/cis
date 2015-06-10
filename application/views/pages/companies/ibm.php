d8e82b590c51a1495fccbce4fc9fc6d71dc3d649
Iain White <iain@whiteinternet.com>
2015-06-10 14:44:10 +1000

    New Pages

---
+++ application/views/pages/companies/ibm.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('IBM'); ?>
+        <p>The <strong>International Business Machines Corporation</strong> (<strong>IBM</strong>) is an American multinational technology and consulting corporation, with headquarters in Armonk, New York, United States.</p>
+        <p>IBM manufactures and markets computer hardware and software, and offers infrastructure, hosting and consulting services in areas ranging from mainframe computers to nanotechnology.</p>
+        <p>The company was founded in 1911 as the <strong>Computing Tabulating Recording Company</strong> (<strong>CTR</strong>) through a merger of three companies: the Tabulating Machine Company, the International Time Recording Company, and the Computing Scale Company.</p>
+        <p>CTR adopted the name International Business Machines in 1924, using a name previously designated to CTR's subsidiary in Canada and later South America.</p>
+        <p>Securities analysts nicknamed IBM <strong><em>Big Blue</em></strong> in recognition of IBM's common use of blue in products, packaging, and logo.</p>
+    </div>
+</div>
