6147009a952063c7ba03628dceaf62bcd76acd3f
Iain White <iain@whiteinternet.com>
2015-06-11 15:58:04 +1000

    TCP/IP

---
+++ application/views/pages/it/networks/ip-suite/application/xmpp.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('XMPP - Extensible Messaging and Presence Protocol'); ?>
+        <p><strong>Extensible Messaging and Presence Protocol</strong> (<strong>XMPP</strong>) is a communications protocol for message-oriented middleware based on <?php echo $this->stencil->addLink('XML', 'WD'); ?> (<strong>Extensible Mark-up Language</strong>).</p>
+        <p>The protocol was originally named <strong>Jabber</strong>, and was developed by the Jabber open-source community in 1999 for near real-time, instant messaging (IM), presence information, and contact list maintenance.</p>
+        <p>Designed to be extensible, the protocol has also been used for publish-subscribe systems, signalling for VoIP, video, file transfer, gaming, Internet of Things applications such as the smart grid, and social networking services.</p>
+        <p>Unlike most instant messaging protocols, XMPP is defined in an open standard and uses an open systems approach of development and application, by which anyone may implement an XMPP service and interoperate with other organizations' implementations.</p>
+        <p>Because XMPP is an open protocol, implementations can be developed using any software license; although many server, client, and library implementations are distributed as free and open-source software, numerous freeware and commercial software implementations also exist.</p>
+        <p>The Internet Engineering Task Force (<?php echo $this->stencil->addLink('IETF', '/companies'); ?>) formed an XMPP working group in 2002 to formalize the core protocols as an IETF instant messaging and presence technology.</p>
+        <p>The XMPP Working group produced four specifications (<?php echo $this->stencil->addRFCLink('3920'); ?>, <?php echo $this->stencil->addRFCLink('3921'); ?>, <?php echo $this->stencil->addRFCLink('3922'); ?>, <?php echo $this->stencil->addRFCLink('3923'); ?>), which were approved as Proposed Standards in 2004. In 2011, <?php echo $this->stencil->addRFCLink('3920'); ?> and <?php echo $this->stencil->addRFCLink('3921'); ?> were superseded by <?php echo $this->stencil->addRFCLink('6120'); ?> and <?php echo $this->stencil->addRFCLink('6121'); ?> respectively, with <?php echo $this->stencil->addRFCLink('6122'); ?> specifying the XMPP address format.</p>
+        <p>In addition to these core protocols standardized at the IETF, the XMPP Standards Foundation (formerly the Jabber Software Foundation) is active in developing open XMPP extensions.</p>
+    </div>
+</div>
