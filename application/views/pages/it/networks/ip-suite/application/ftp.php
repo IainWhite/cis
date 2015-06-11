e2342f634d7da35e0f1dcab8a01379194d85b394
Iain White <iain@whiteinternet.com>
2015-06-11 13:41:59 +1000

    TCP/IP

--- application/views/pages/it/networks/ip-suite/application/ftp.php
+++ application/views/pages/it/networks/ip-suite/application/ftp.php
@@ -1,12 +1,12 @@
 <div class="row">
     <div class="col-lg-12 col-md-12">
         <?php echo $this->stencil->setHeadline('FTP - File Transfer Protocol'); ?>
-        <p>The <strong>File Transfer Protocol</strong> (<strong>FTP</strong>) is a standard network protocol used to transfer computer files from one host to another host over a <?php echo $this->stencil->addLink('TCP', '/it/networks/internet-protocol-suite'); ?>-based network, such as the <?php echo $this->stencil->addLink('Internet', '/it/networks'); ?>.</p>
+        <p>The <strong>File Transfer Protocol</strong> (<strong>FTP</strong>) is a standard network protocol used to transfer computer files from one host to another host over a <?php echo $this->stencil->addLink('TCP', '/it/networks/internet-protocol-suite/transfer'); ?>-based network, such as the <?php echo $this->stencil->addLink('Internet', '/it/networks'); ?>.</p>
         <p>FTP is built on a <?php echo $this->stencil->addLink('client-server', '/it/networks'); ?> architecture and uses separate control and data connections between the client and the server.</p>
         <p>FTP users may authenticate themselves using a clear-text sign-in protocol, normally in the form of a username and password, but can connect anonymously if the server is configured to allow it.</p>
         <p><strong>SSH File Transfer Protocol</strong> (SFTP) is sometimes also used instead, but is technologically different.</p>
         <p>The first FTP client applications were command-line applications developed before operating systems had graphical user interfaces, and are still shipped with most <?php echo $this->stencil->addLink('Windows', '/it/operating-systems'); ?>, <?php echo $this->stencil->addLink('Unix', '/it/operating-systems'); ?>, and <?php echo $this->stencil->addLink('Linux', '/it/operating-systems'); ?> operating systems.</p>
         <p>Many FTP clients and automation utilities have since been developed for desktops, servers, mobile devices, and hardware, and FTP has been incorporated into productivity applications, such as web page editors.</p>
-        <p>The original specification for the File Transfer Protocol was written by <strong>Abhay Bhushan</strong> and published as <?php echo $this->stencil->addRFCLink('114'); ?> on 16 April 1971. Until 1980, FTP ran on NCP, the predecessor of <?php echo $this->stencil->addLink('TCP/IP', '/it/networks/internet-protocol-suite'); ?>.</p>
+        <p>The original specification for the File Transfer Protocol was written by <strong>Abhay Bhushan</strong> and published as <?php echo $this->stencil->addRFCLink('114'); ?> on 16 April 1971. Until 1980, FTP ran on NCP, the predecessor of <?php echo $this->stencil->addLink('TCP/IP', '/it/networks'); ?>.</p>
     </div>
 </div>
---
+++ application/views/pages/it/networks/ip-suite/internet/ip.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('IP - Internet Protocol'); ?>
+        <p>The <strong>Internet Protocol</strong> (<strong>IP</strong>) is the principal communications protocol in the <?php echo $this->stencil->addLink('Internet Protocol Suite', '/it/networks/internet-protocol-suite'); ?> for relaying datagrams across network boundaries.</p>
+        <p>Its routing function enables internetworking, and essentially establishes the Internet.</p>
+        <p>IP, as the primary protocol in the <?php echo $this->stencil->addLink('Internet Layer', '/it/networks/internet-protocol-suite'); ?> of the Internet protocol suite, has the task of delivering packets from the source host to the destination host solely based on the IP addresses in the packet headers.</p>
+        <p>For this purpose, IP defines packet structures that encapsulate the data to be delivered.</p>
+        <p>It also defines addressing methods that are used to label the datagram with source and destination information.</p>
+        <p>Historically, IP was the connectionless datagram service in the original Transmission Control Program introduced by Vint Cerf and Bob Kahn in 1974; the other being the connection-oriented Transmission Control Protocol (<?php echo $this->stencil->addLink('TCP', '/it/networks/internet-protocol-suite/transport'); ?>).</p>
+        <p>The Internet protocol suite is therefore often referred to as <?php echo $this->stencil->addLink('TCP/IP', '/it/networks'); ?>.</p>
+        <p>The first major version of IP, Internet Protocol Version 4 (IPv4), is the dominant protocol of the Internet. Its successor is Internet Protocol Version 6 (IPv6).</p>
+     </div>
+</div>
