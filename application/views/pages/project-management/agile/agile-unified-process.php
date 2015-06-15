51f7fa0f702b9144f867fa16847749fc928134f1
Iain White <iain@whiteinternet.com>
2015-06-15 18:00:17 +1000

    Iain White

---
+++ application/views/pages/project-management/agile/agile-unified-process.php
@@ -0,0 +1,6 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Agile Unified Process (AUP)'); ?>
+        <p><strong>Agile Unified Process (AUP)</strong> is a simplified version of the <?php echo $this->stencil->addLink('Rational Unified Process', 'PM'); ?> (RUP) developed by Scott Ambler. It describes a simple, easy to understand approach to developing business application software using agile techniques and concepts yet still remaining true to the RUP. The AUP applies agile techniques including <?php echo $this->stencil->addLink('Test-Driven Development', 'PMA'); ?> (TDD), <?php echo $this->stencil->addLink('Agile Modeling', 'PMA'); ?> (AM), agile change management, and database refactoring to improve productivity.</p>
+    </div>
+</div>
---
+++ application/views/pages/project-management/rational-unified-process.php
@@ -0,0 +1,6 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('The Rational Unified Process (RUP)'); ?>
+        <p>The <strong>Rational Unified Process (RUP)</strong> is an iterative software development process framework created by the <strong>Rational Software Corporation</strong>, a division of <?php echo $this->stencil->addLink('IBM', 'C'); ?> since 2003. RUP is not a single concrete prescriptive process, but rather an adaptable process framework, intended to be tailored by the development organisations and software project teams that will select the elements of the process that are appropriate for their needs. RUP is a specific implementation of the <?php echo $this->stencil->addLink('Unified Process', 'PM'); ?>.</p>
+    </div>
+</div>
