887bae03d8829c529063828f862cba73e34f096f
Iain White <iain@whiteinternet.com>
2015-06-16 17:56:48 +1000

    New Pages

---
+++ application/views/pages/people/larry-wall.php
@@ -0,0 +1,12 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Larry Wall'); ?>
+        <p><strong>Larry Wall</strong> (born 1954), whose graduate work was in linguistics, designed <?php echo $this->stencil->addLink('Perl', 'L'); ?> in 1987 for reports processing and continues to oversee the language’s development according to the motto "<em>Larry is always right, even when he was wrong.</em>"</p>
+        <p>He also originated the three canonical "<em>virtues</em>" of a good programmer: <em>laziness, impatience, and hubris</em>.</p>
+        <p>Larry is also known as the original author of the rn Usenet software, and the nearly universally used patch. He has won the IOCCC twice, and was the recipient of the first Free Software Foundations award for the Advancement of Free Software in 1998.</p>
+        <p>Beyond his technical skills, Larry is known for his wit and often ironic sense of humour which he displays in the comments to his source code or on Usenet (e.g. "<em>We all agree on the necessity of compromise. We just can’t agree on when it’s necessary to compromise</em>"</p>
+        <p>Larry Wall is a trained linguist, which helped him with his book writing, as well as with the design of Perl. He is the co-author of <strong>Programming Perl</strong> (often referred to as the <em>Camel Book</em>), which is the definitive resource for Perl programmers. He has also edited the <strong>Perl Cookbook</strong>.</p>
+        <p>Larry continues to oversee further development of Perl and serves as the <strong>Benevolent Dictator for Life</strong> of the Perl project.</p>
+        <p>Larry’s favourite colour is chartreuse.</p>
+    </div>
+</div>
---
+++ application/views/pages/people/pei-yuan-wei.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Pei-Yuan Wei'); ?>
+        <p><strong>Pei-Yuan Wei</strong> is the creator of <?php echo $this->stencil->addLink('ViolaWWW', 'WB'); ?>, the first popular graphical web browser.</p>
+        <p>ei-Yuan Wei was born in Taiwan.</p>
+        <p>He graduated from Berkeley High School in 1986, received his higher education at the University of California, Berkeley, and was a member of a student club called the eXperimental Computer Facility (XCF).</p>
+    </div>
+</div>
