32f44bb49401c3b798f0552a9a21e45d6ee31c74
Iain White <iain@whiteinternet.com>
2015-06-18 10:46:44 +1000

    Update

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,5 +1,29 @@
-<div class="row">
-    <div class="col-lg-12 col-md-12">
-        <?php echo $this->stencil->setHeadline('About'); ?>
+<div class="funny-boxes-v2 rounded-2x shadow-effect-1">
+    <div class="row">
+        <div class="col-lg-12 col-md-12">
+            <?php echo $this->stencil->setHeadline('About'); ?>
+            <h2>About White Internet</h2>
+            <p>This site contains information about Web Development, Project Management, general Information Technology (IT) and is a portfolio and on-line curriculum vitae (CV) also known as a R&eacute;sum&eacute; for Iain White.</p>
+            <p>Iain has had a long career as a Software Developer, specialising in web based applications. Iain is a certified Scrum Master with proven leadership skills.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12 col-md-12">
+            <h2>Site Creation</h2>
+            <p>This site was designed and created by <strong><a href="iain-white">Iain White</a></strong> a <strong>PHP Developer</strong> living in Brisbane, Australia.</p>
+            <p>This web site was developed in <strong>PHP</strong> version <?php echo $this->stencil->getVersion('PHP'); ?> with <strong>MySQL</strong> version <?php echo $this->stencil->getVersion('MySQL'); ?>.</p>
+            <p><strong>jQuery</strong> <span id="jqueryVersion">1.8</span> was used to do much of the clever front-end stuff.</p>
+            <p>The layout is based on the <strong><a href="http://getbootstrap.com" title="Bootstrap" rel="external">Bootstrap 3</a></strong>.</p>
+            <p>This web site is hosted on <strong><a href="http://www.freebsd.org/" title="FreeBSD" rel="external">FreeBSD</a></strong> with <strong><a href="http://www.quadrahosting.com.au/" rel="external">Quadra Hosting</a></strong>.</p>
+            <p>The following tools where used to produce this web site.</p>
+            <ul>
+                <li><strong><a href="http://netbeans.org/" title="NetBeans" rel="external">NetBeans IDE</a></strong>.
+                <li><strong>Adobe Fireworks</strong> CS6.
+                <li><strong><a href="http://getfirebug.com/" title="Firebug" rel="external">Firebug</a></strong> for Firefox.
+                <li><strong><a href="http://www.firephp.org/" title="FirePHP" rel="external">FirePHP</a></strong> for Firefox.
+            </ul>
+            <p>Developed on an iMac running OS X Mavericks and a Dell Vostro 220 running  Windows 7 Professional.</p>
+            <p>This site has been tested on Internet Explorer, Firefox, Safari, Google Chrome, Apple iPad and iPhone.</p>
+        </div>
     </div>
 </div>
--- application/views/pages/privacy-policy.php
+++ application/views/pages/privacy-policy.php
@@ -1,5 +1,45 @@
 <div class="row">
     <div class="col-lg-12 col-md-12">
         <?php echo $this->stencil->setHeadline('Privacy Policy'); ?>
+        <p>This privacy policy sets out how White Internet uses and protects any information that you give White Internet when you use this web site.</p>
+        <p>White Internet is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this web site, then you can be assured that it will only be used in accordance with this privacy statement.</p>
+        <p>White Internet may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes.</p>
+
+        <p class="pull-right"><strong>This policy is effective from 14<sup>th</sup> February 2006.</strong></p>
+
+        <h2>What we collect</h2>
+        <p>We may collect the following information:</p>
+        <ul>
+            <li>Browser and OS type.</li>
+            <li>IP address.</li>
+            <li>Contact information including name, company and e-mail address.</li>
+        </ul>
+
+        <h2>What we do with the information we gather</h2>
+        <p>We require this information to understand your needs and provide you with a better service, and in particular for the following reasons:</p>
+        <ul>
+            <li>Internal record keeping.</li>
+            <li>We may use the information to improve our products and services.</li>
+            <li>We may use the information to customise the web site.</li>
+            <li>We will not provide your information to third party any partners.</li>
+            <li>We will never sell your information.</li>
+        </ul>
+
+        <h2>Security</h2>
+        <p>We are committed to ensuring that your information is secure. In order to prevent unauthorised access or disclosure we have put in place suitable physical, electronic and managerial procedures to safeguard and secure the information we collect online.</p>
+
+        <h2>How we use cookies</h2>
+        <p>A cookie is a small file which asks permission to be placed on your computer's hard drive.</p>
+        <p>Once you agree, the file is added and the cookie helps analyse web traffic or lets you know when you visit a particular site. Cookies allow web applications to respond to you as an individual. The web application can tailor its operations to your needs, likes and dislikes by gathering and remembering information about your preferences.</p>
+        <p>We use traffic log cookies to identify which pages are being used. This helps us analyse data about web page traffic and improve our web site in order to tailor it to customer needs. We only use this information for statistical analysis purposes and then the data is removed from the system.</p>
+        <p>Overall, cookies help us provide you with a better web site, by enabling us to monitor which pages you find useful and which you do not. A cookie in no way gives us access to your computer or any information about you, other than the data you choose to share with us.</p>
+        <p>You can choose to accept or decline cookies. Most web browsers will automatically accept cookies, but you can usually modify your browser settings to decline cookies, if you prefer. This may prevent you from taking full advantage of the web site.</p>
+
+        <h2>Links to other web sites</h2>
+        <p>Our web site may contain links to enable you to visit other web sites of interest easily. However, once you have used these links to leave our site, you should note that we do not have any control over that other web site. Therefore, we cannot be responsible for the protection and privacy of any information which you provide whilst visiting such sites and such sites are not governed by this privacy statement. You should exercise caution and look at the privacy statement applicable to the web site in question.</p>
+
+        <h2>Controlling your personal information</h2>
+        <p>You may request details of personal information which we hold about you under the Data Protection Act 1998. A small fee will be payable. If you would like a copy of the information held on you please send an e-mail to [dpa@whiteinternet.com].</P>
+        <p>If you believe that any information we are holding on you is incorrect or incomplete, please e-mail us as soon as possible, at the above address. We will promptly correct any information found to be incorrect.</p>
     </div>
 </div>
