f84301ea1ee27aa73e5f823fa60512b83f6c8e6c
Iain White <iain@whiteinternet.com>
2015-06-25 17:57:57 +1000

    New Pages

---
+++ application/views/pages/web-development/browsers/ie/internet-explorer-6.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('IE6 - Internet Explorer 6'); ?>
+        <p><?php echo $this->stencil->addLink('Internet Explorer', 'WB'); ?> is a <?php echo $this->stencil->addLink('Web browser', 'WD'); ?> developed by <?php echo $this->stencil->addLink('Microsoft', 'C'); ?>.</p>
+        <p><strong>Microsoft Internet Explorer 6</strong> was released on 27 August 2001, a few months before <?php echo $this->stencil->addLink('Windows XP', 'WIN'); ?>.</p>
+        <p>This version included <?php echo $this->stencil->addLink('DHTML', 'WD'); ?> enhancements, content restricted in-line frames, and partial support of <?php echo $this->stencil->addLink('CSS', 'WD'); ?> level 1, <?php echo $this->stencil->addLink('DOM', 'WD'); ?> level 1, and <?php echo $this->stencil->addLink('SMIL', 'WD'); ?> 2.0.</p>
+        <p>Other new features included a new version of the <?php echo $this->stencil->addLink('Internet Explorer Administration Kit', 'web-development/browsers/ie/add-ons'); ?> (IEAK)</strong>, Media bar, Windows Messenger integration, fault collection, automatic image resizing, P3P, and a new look-and-feel that was in line with the Luna visual style of Windows XP, when used in Windows XP.</p>
+        <p>Internet Explorer 6.0 SP1 offered several security enhancements and coincided with the Windows XP SP1 patch release.</p>
+        <p>In 2002, the <?php echo $this->stencil->addLink('Gopher', 'IPA'); ?> protocol was disabled, and support for it was dropped in <?php echo $this->stencil->addLink('Internet Explorer 7', 'IE'); ?>.</p>
+        <p>Internet Explorer 6.0 SV1 came out on 6 August 2004 for Windows XP SP2 and offered various security enhancements and new colour buttons on the user interface.</p>
+        <p>Internet Explorer 6 updated the original 'blue e' logo to a lighter blue and more 3D look.</p>
+    </div>
+</div>
---
+++ application/views/pages/web-development/browsers/ie/internet-explorer-8.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('IE8 - Internet Explorer 8'); ?>
+        <p><?php echo $this->stencil->addLink('Internet Explorer', 'WB'); ?> is a <?php echo $this->stencil->addLink('Web browser', 'WD'); ?> developed by <?php echo $this->stencil->addLink('Microsoft', 'C'); ?>.</p>
+        <p><strong>Microsoft Internet Explorer 8</strong> was released on 19 March 2009.</p>
+        <p>It had been in development since August 2007. On 5 March 2008, the first public beta (Beta 1) was released to the general public. On 27 August 2008, the second public beta (Beta 2) was released.</p>
+        <p>It is supported in <?php echo $this->stencil->addLink('Windows XP', 'WIN'); ?> SP2 and SP3, Windows Server 2003 SP2, <?php echo $this->stencil->addLink('Windows Vista', 'WIN'); ?>, <?php echo $this->stencil->addLink('Windows 7', 'WIN'); ?>, and Windows Server 2008 on both 32-bit and 64-bit architectures.</p>
+        <p>Internet Explorer 8 (IE8) RC1 was released on 26 January 2009. Internet Explorer 8 "Final" was released on 19 March 2009.</p>
+        <p>Security, ease of use, and improvements in <?php echo $this->stencil->addLink('RSS', 'WD'); ?>, <?php echo $this->stencil->addLink('CSS', 'WD'); ?>, and <?php echo $this->stencil->addLink('AJAX', 'WD'); ?> support are Microsoft's priorities for IE8.</p>
+        <p>It includes much stricter compliance with web standards, including a planned full Cascading Style Sheets 2.1 compliance for the release version.</p>
+        <p>All of these changes allowed Internet Explorer 8 to pass the <strong>Acid2</strong> test. However, to prevent compatibility issues, IE8 also includes the <?php echo $this->stencil->addLink('IE7', 'IE'); ?> rendering behaviour.</p>
+        <p>Sites that expect IE7 quirks can disable IE8's breaking changes by including a meta element in the <code>HEAD</code> section of the <?php echo $this->stencil->addLink('HTML', 'WD'); ?> document.</p>
+        <p>IE8 also includes numerous improvements to <?php echo $this->stencil->addLink('JavaScript', 'L'); ?> support as well as performance improvements, although it still does not pass the <strong>Acid3</strong> test.</p>
+        <p>It includes support for accelerators, which allow supported web applications to be invoked without explicitly navigating to them; and WebSlices, which allows portions of a page to be subscribed to and monitored from a redesigned Favorites Bar.</p>
+        <p>Other features include InPrivate privacy features and the SmartScreen Filter.</p>
+    </div>
+</div>
