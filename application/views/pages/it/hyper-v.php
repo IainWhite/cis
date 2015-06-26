939e51f6fd6c3b5fb03f0649c89af1abfa335d2a
Iain White <iain@whiteinternet.com>
2015-06-26 13:46:44 +1000

    New Pages

---
+++ application/views/pages/it/hyper-v.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Hyper-V'); ?>
+        <p><strong>Hyper-V</strong>, codenamed <em>Viridian</em> and formerly known as <strong>Windows Server Virtualization</strong>, is a native hypervisor; it can create virtual machines on x86-64 systems. Starting with <?php echo $this->stencil->addLink('Windows 8', 'WIN'); ?>, Hyper-V supersedes <?php echo $this->stencil->addLink('Windows Virtual PC', 'IT'); ?> as the hardware virtualisation component of the client editions of Windows NT. A server computer running Hyper-V can be configured to expose individual virtual machines to one or more networks.</p>
+        <p>Hyper-V was first released alongside <?php echo $this->stencil->addLink('Windows Server 2008', 'WIS'); ?> and has since become a staple of the <?php echo $this->stencil->addLink('Windows Server', 'OS'); ?> family.</p>
+    </div>
+</div>
---
+++ application/views/pages/it/os/windows-server.php
@@ -0,0 +1,21 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Windows Server'); ?>
+        <p><strong>Windows Server</strong> is a brand name for a group of server <?php echo $this->stencil->addLink('Operating Systems', 'IT'); ?> released by <?php echo $this->stencil->addLink('Microsoft', 'C'); ?>.</p>
+        <p>It includes all <?php echo $this->stencil->addLink('Windows', 'OS'); ?> operating systems that have "Windows Server" in their names, but not any other Microsoft product.</p>
+
+        <p>This brand includes the following operating systems:</p>
+        <ul>
+            <li><?php echo $this->stencil->addLink('Windows Server 2003', 'WIS'); ?> (April 2003)</li>
+            <li><?php echo $this->stencil->addLink('Windows Server 2003 R2', 'WIS'); ?> (December 2005)</li>
+            <li><?php echo $this->stencil->addLink('Windows Server 2008', 'WIS'); ?> (February 2008)</li>
+            <li><?php echo $this->stencil->addLink('Windows Server 2008 R2', 'WIS'); ?> (July 2009)</li>
+            <li><?php echo $this->stencil->addLink('Windows Server 2012', 'WIS'); ?> (August 2012)</li>
+            <li><?php echo $this->stencil->addLink('Windows Server 2012 R2', 'WIS'); ?> (October 2013)</li>
+        </ul>
+
+        <p>Microsoft has also produced <?php echo $this->stencil->addLink('Windows Server Essentials', 'WIS'); ?> (formerly <strong>Windows Small Business Server</strong>) and <?php echo $this->stencil->addLink('Windows Essential Business Server', 'WIS'); ?> (discontinued), software bundles which includes a Windows Server operating system and some other Microsoft Servers products.</p>
+
+        <p>Older server operating systems: <?php echo $this->stencil->addLink('Windows NT 3.1', 'WIS'); ?>, <?php echo $this->stencil->addLink('Windows NT 3.5', 'WIS'); ?>, <?php echo $this->stencil->addLink('Windows NT 3.15', 'WIS'); ?>, <?php echo $this->stencil->addLink('Windows NT 4.0', 'WIS'); ?>, <?php echo $this->stencil->addLink('Windows 2000 Server', 'WIS'); ?> and <?php echo $this->stencil->addLink('Windows Home Server', 'WIS'); ?>.</p>
+    </div>
+</div>
