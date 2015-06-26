b369e17ac6f18e6c9281f94a6b2beee7993965f3
Iain White <iain@whiteinternet.com>
2015-06-26 16:25:37 +1000

    New Pages

---
+++ application/views/pages/it/os/os-x/mac-os-x-panther.php
@@ -0,0 +1,23 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Mac OS X Panther'); ?>
+        <p><strong>Mac OS X Panther</strong> (version 10.3) is the fourth major release of <?php echo $this->stencil->addLink('Mac OS X', 'OS'); ?>, <?php echo $this->stencil->addLink('Apples', 'C'); ?>’s desktop and server operating system. It followed <?php echo $this->stencil->addLink('MacOS X 10.2', 'OSX'); ?> Jaguar and preceded <?php echo $this->stencil->addLink('Mac OS X 10.4', 'OSX'); ?> Tiger. Apple released Panther on 24 October 2003.</p>
+        <p>Apple advertised that Mac OS X Panther had over 150 new features, including:</p>
+        <ul>
+            <li>Finder: Updated with a brushed-metal interface, a new live search engine, customizable Sidebar, secure deletion, File labels and Zip support built in. The Finder icon was also changed.</li>
+            <li>Fast User Switching: Allows a user to remain logged in while another user logs in</li>
+            <li>Exposé: Helps the user manage windows by showing them all as thumbnails.</li>
+            <li>TextEdit: TextEdit now is also compatible with Microsoft Word (.doc) documents.</li>
+            <li>Xcode developer tools: Faster compile times with gcc 3.3.</li>
+            <li>Preview: Increased speed with PDF rendering</li>
+            <li>QuickTime: Now supports the Pixlet high definition video codec</li>
+            <li>Font Book: Font Book, Apple's font manager, simplifies viewing character maps and installing fonts. It also allows the user to sort fonts into categories.</li>
+            <li>FileVault: On the fly encryption and decryption of a user’s home folder</li>
+            <li>iChat AV: The new version of iChat. Now with built-in Audio- and video-conferencing.</li>
+            <li>X11: X11 is built into Panther</li>
+            <li><?php echo $this->stencil->addLink('Safari', 'WB'); ?>: New browser made to replace <?php echo $this->stencil->addLink('Internet Explorer for Mac', 'IE'); ?>, developed when the contract between Apple and Microsoft ended. Internet Explorer for Mac was still available. Safari 1.0 was included in an update in Jaguar but was used as the default browser in Panther.</li>
+            <li>Microsoft Windows interoperability improvements, including out-of-the-box support for Active Directory and SecurID-based VPNs.</li>
+            <li>Built-in fax support.</li>
+        </ul>
+    </div>
+</div>
