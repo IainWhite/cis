557ddacf52e20ed36094de2c6463e3b6d683ec65
Iain White <iain@whiteinternet.com>
2015-06-29 17:58:58 +1000

    New Pages

---
+++ application/views/pages/it/software/databases/sqlite.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('SQLite'); ?>
+        <p><strong>SQLite</strong> is a Relational Database Management System (<?php echo $this->stencil->addLink('RDBMS', 'DB'); ?>) contained in a <?php echo $this->stencil->addLink('C', 'L'); ?> programming library.</p>
+        <p>In contrast to other database management systems, SQLite is not a separate process that is accessed from the client application, but an integral part of it.</p>
+        <p>SQLite is <?php echo $this->stencil->addLink('ACID', 'DB'); ?> compliant and implements most of the <?php echo $this->stencil->addLink('SQL', 'DB'); ?> standard, using a dynamically and weakly typed SQL syntax that does not guarantee the domain integrity.</p>
+    </div>
+</div>
