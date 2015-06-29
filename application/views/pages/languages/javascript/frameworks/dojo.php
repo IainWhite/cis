5fab2acba0695bebf829346a080eace6e9475c28
Iain White <iain@whiteinternet.com>
2015-06-29 13:00:20 +1000

    New Page

---
+++ application/views/pages/languages/javascript/frameworks/dojo.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Dojo JavaScript Toolkit'); ?>
+        <p><strong>Dojo Toolkit</strong> is an open source modular <?php echo $this->stencil->addLink('JavaScript', 'L'); ?>  library (or more specifically JavaScript toolkit) designed to ease the rapid development of cross-platform, JavaScript / Ajax-based applications and web sites. It was started by <strong>Alex Russell</strrong>, <strong>Dylan Schiemann</strong>, <strong>David Schontzler</strong>, and others in 2004 and is dual-licensed under the modified BSD license or the Academic Free License. The <strong>Dojo Foundation</strong> is a non-profit organisation created with the goal to promote the adoption of the toolkit.</p>
+    </div>
+</div>
+
