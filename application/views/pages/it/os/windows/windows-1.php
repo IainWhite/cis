b7e49cad81910af054fe8c80de7ac8fc445da490
Iain White <iain@whiteinternet.com>
2015-06-30 13:01:17 +1000

    Update

---
+++ application/views/pages/it/os/windows/windows-1.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('MS Windows 1.0'); ?>
+        <p><strong>Microsoft Windows 1.0</strong> was a 16-bit graphical operating environment, developed by <?php echo $this->stencil->addLink('Microsoft Corporation', 'C'); ?> and released on 20 November 1985.</p>
+        <p>It was Microsoft's first attempt to implement a multi-tasking graphical user interface-based operating environment on the PC platform.</p>
+        <p>Microsoft first presented Windows to the public on 10 November 1983. Requiring two floppy disk drives 192KB of RAM, Microsoft described the software as a device driver for <?php echo $this->stencil->addLink('MS-DOS', 'DOS'); ?> 2.0. By supporting cooperative multitasking in tiled windows when using well-behaved applications that only used <?php echo $this->stencil->addLink('DOS', 'OS'); ?> system calls, and permitting non well-behaved applications to run in a full screen.</p>
+    </div>
+</div>
---
+++ application/views/pages/it/os/windows/windows-2.php
@@ -0,0 +1,23 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('MS Windows 2.x'); ?>
+        <p><strong>Windows 2.0</strong> is a 16-bit <?php echo $this->stencil->addLink('Microsoft', 'C'); ?> <?php echo $this->stencil->addLink('Windows', 'OS'); ?> <?php echo $this->stencil->addLink('GUI', 'IT'); ?> based operating environment that was released on 9 December 1987 and is the successor to <?php echo $this->stencil->addLink('Windows 1', 'WIN'); ?>.</p>
+        <p>Windows 2.0 allowed application windows to overlap each other unlike its predecessor Windows 1.0, which could display only tiled windows.</p>
+        <p>Windows 2.0 also introduced more sophisticated keyboard-shortcuts and the terminology of "<em>Minimize</em>" and "<em>Maximize</em>", as opposed to "<em>Iconize</em>" and "<em>Zoom</em>" in Windows 1.0.</p>
+        <p>New features in Windows 2.0 included <strong>VGA</strong> graphics (although 16 colors only). It was also the last version of Windows that did not require a hard disk.</p>
+        <p>Windows 2.0 was also the first Windows version to integrate the control panel.</p>
+    </div>
+</div>
+
+<?php if (!$this->stencil->getIsSub()): ?>
+    <div class="row">
+        <div class="col-lg-12 col-md-12">
+            <h2>Windows 2.1x</h2>
+            <p><strong>Windows/286 2.10</strong> and <strong>Windows/386 2.10</strong> were released on 27 May 1988, less than a year after the release of <?php echo $this->stencil->addLink('Windows 2', 'WIN'); ?>. These versions can take advantage of the specific features of the <?php echo $this->stencil->addLink('Intel 80286', 'CPU'); ?> and <?php echo $this->stencil->addLink('Intel 80386', 'CPU'); ?> processors. A hard disk was required for the first time to install Windows.</p>
+
+            <h2>Windows 2.11</h2>
+            <p>On 13 March 1989, <strong>Windows 2.11</strong> was released in <strong>Windows/286</strong> and <strong>Windows/386</strong> editions, with some minor changes in memory management, <strong>AppleTalk</strong> support and faster printing and updated printer drivers.</p>
+            <p>Windows 2.11 was superseded by <?php echo $this->stencil->addLink('Windows 3', 'WIN'); ?> in May 1990, but was supported by Microsoft for twelve years, until 31 December 2001.</p>
+        </div>
+    </div>
+<?php endif; ?>
