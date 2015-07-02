9bd2ea57b9d89cd65c3a3a24942c6b59c2934d75
Iain White <iain@whiteinternet.com>
2015-07-02 15:37:21 +1000

    Fix IDE

--- application/views/pages/it/software/ide.php
+++ application/views/pages/it/software/ide.php
@@ -1,9 +1,12 @@
 <div class="row">
     <div class="col-lg-12 col-md-12">
-        <?php echo $this->stencil->setHeadline('Compiler'); ?>
-        <p>A <strong>compiler</strong> is a computer program (or set of programs) that transforms source code written in a programming language (the source language) into another computer language (the target language, often having a binary form known as object code). The most common reason for converting a source code is to create an executable program.</p>
-        <p>The name "compiler" is primarily used for programs that translate source code from a high-level programming language to a lower level language (e.g., [assembly language] or [machine code]). If the compiled program can run on a computer whose CPU or operating system is different from the one on which the compiler runs, the compiler is known as a cross-compiler. More generally, compilers are a specific type of translators.</p>
-        <p>A program that translates from a low level language to a higher level one is a decompiler. A program that translates between high-level languages is usually called a source-to-source compiler or transpiler. A language rewriter is usually a program that translates the form of expressions without a change of language. The term compiler-compiler is sometimes used to refer to a parser generator, a tool often used to help create the lexer and parser.</p>
-        <p>A compiler is likely to perform many or all of the following operations: lexical analysis, preprocessing, parsing, semantic analysis (syntax-directed translation), code generation, and code optimization. Program faults caused by incorrect compiler behaviour can be very difficult to track down and work around; therefore, compiler implementors invest significant effort to ensure compiler correctness.</p>
+        <?php echo $this->stencil->setHeadline('IDE - Integrated Development Environment'); ?>
+        <p>An <strong>Integrated Development Environment</strong> (<strong>IDE</strong>) is a software application that provides comprehensive facilities to computer programmers for software development.</p>
+        <p>An IDE normally consists of a source code editor, build automation tools and a debugger.</p>
+        <p>Most modern IDEs offer Intelligent code completion features.</p>
+        <p>Some IDEs contain a compiler, interpreter, or both, such as <?php echo $this->stencil->addLink('NetBeans', 'SWI'); ?> and <?php echo $this->stencil->addLink('Eclipse', 'SWi'); ?> others do not, such as SharpDevelop and Lazarus.</p>
+        <p>The boundary between an integrated development environment and other parts of the broader software development environment is not well-defined.</p>
+        <p>Sometimes a <?php echo $this->stencil->addLink('version control', 'IT'); ?> system and various tools are integrated to simplify the construction of a <?php echo $this->stencil->addLink('GUI', 'IT'); ?>.</p>
+        <p>Many modern IDEs also have a class browser, an object browser, and a class hierarchy diagram, for use in object-oriented software development.</p>
     </div>
 </div>
