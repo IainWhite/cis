55902c928121db3e8a14ff8a071495b9f1df060f
Iain White <iain@whiteinternet.com>
2015-07-02 17:44:18 +1000

    New Pages

---
+++ application/views/pages/languages/visual-foxpro.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Visual FoxPro'); ?>
+        <p><strong>Visual FoxPro</strong> is a data-centric object-oriented procedural programming language produced by <?php echo $this->stencil->addLink('Microsoft', 'C'); ?>.</p>
+        <p>It is derived from <?php echo $this->stencil->addLink('FoxPro', 'DB'); ?> (originally known as <strong>FoxBASE</strong>) which was developed by <strong>Fox Software</strong> beginning in 1984.</p>
+        <p>Fox Technologies merged with Microsoft in 1992, after which the software acquired further features and the prefix "<em>Visual</em>".</p>
+        <p>The last version of FoxPro (2.6) worked under <?php echo $this->stencil->addLink('Mac OS', 'OS'); ?>, <?php echo $this->stencil->addLink('DOS', 'OS'); ?>, <?php echo $this->stencil->addLink('Windows', 'OS'); ?>, and <?php echo $this->stencil->addLink('Unix', 'OS'); ?>.</p>
+        <p>Visual FoxPro 3.0, the first "Visual" version, reduced platform support to only Mac and Windows, and later versions were Windows only.</p>
+    </div>
+</div>
---
+++ application/views/pages/web-development/browsers/firefox/mozilla-web-developer.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Mozilla Web Developer'); ?>
+        <p><strong>Web Developer</strong> is an extension for <?php echo $this->stencil->addLink('Mozilla', 'WB'); ?> based web browsers that adds editing and debugging tools for web developers.</p>
+        <p>It has been tested to be compatible with <?php echo $this->stencil->addLink('Firefox', 'WB'); ?>, <?php echo $this->stencil->addLink('Flock', 'WB'); ?>, and <?php echo $this->stencil->addLink('SeaMonkey', 'WB'); ?>.</p>
+        <p>The extension was developed by <strong>Chris Pederick</strong>, who has also authored the User Agent Switcher extension.</p>
+        <p>Web Developer was a grand prize winner in Mozilla's 2006 "Extend Firefox" competition and it is one of the few extensions specifically recommended by the Firefox developers.</p>
+    </div>
+</div>
