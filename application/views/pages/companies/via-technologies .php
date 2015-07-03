664374d426083c04a1ebfe60f7e38399c053f674
Iain White <iain@whiteinternet.com>
2015-07-03 16:30:45 +1000

    New Pages

---
+++ application/views/pages/companies/via-technologies .php
@@ -0,0 +1,6 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('VIA Technologies '); ?>
+        <p><strong>VIA Technologies Inc</strong> is a Taiwanese manufacturer of integrated circuits, mainly motherboard chipsets, CPUs, and memory, and is part of the Formosa Plastics Group. It is the world's largest independent manufacturer of motherboard chipsets. As a fabless semiconductor company, VIA conducts research and development of its chipsets in-house, then subcontracts the actual (silicon) manufacturing to third-party merchant foundries, such as TSMC.</p>
+    </div>
+</div>
---
+++ application/views/pages/it/hardware/cpu/intel/itanium.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Intel Itanium'); ?>
+        <p><strong>Itanium</strong> is a family of 64-bit <?php echo $this->stencil->addLink('Intel', 'C'); ?> microprocessors that implement the Intel Itanium architecture (formerly called IA-64). Intel markets the processors for enterprise servers and high-performance computing systems. The Itanium architecture originated at Hewlett-Packard (<?php echo $this->stencil->addLink('HP', 'C'); ?>), and was later jointly developed by HP and Intel.</p>
+        <p>Itanium based systems have been produced by HP (the HP Integrity Servers line) and several other manufacturers. As of 2008, Itanium was the fourth most deployed microprocessor architecture for enterprise class systems, behind <?php echo $this->stencil->addLink('x86-64', 'CPU'); ?>, Power Architecture, and SPARC.</p>
+    </div>
+</div>
