a2ec62f7238068d32d0562ca1a01a05445478092
Iain White <iain@whiteinternet.com>
2015-07-08 14:47:30 +1000

    New Page

---
+++ application/views/pages/it/hardware/cpu/microarchitecture.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Microarchitecture'); ?>
+        <p>In computer engineering and computer science, <strong>microarchitecture</strong>, also called computer organisation, is the way a given Instruction Set Architecture (<?php echo $this->stencil->addLink('ISA', 'ITH'); ?>) is implemented on a processor A given ISA may be implemented with different microarchitectures; implementations may vary due to different goals of a given design or due to shifts in technology.</p>
+        <p><?php echo $this->stencil->addLink('Computer architecture', 'ITH'); ?> is the combination of microarchitecture and instruction set designs.</p>
+    </div>
+</div>
