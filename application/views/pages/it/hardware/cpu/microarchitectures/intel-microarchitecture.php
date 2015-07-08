085ee81d28e913f5405b0c3a69f3be00ae022a86
Iain White <iain@whiteinternet.com>
2015-07-08 16:43:49 +1000

    New Pages

---
+++ application/views/pages/it/hardware/cpu/microarchitectures/intel-microarchitecture.php
@@ -0,0 +1,56 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Intel x86 Microarchitecture'); ?>
+        <ul>
+            <li><?php echo $this->stencil->addLink('8086', 'CPU'); ?>: first x86 processor; initially a temporary substitute for the iAPX 432 to compete with <?php echo $this->stencil->addLink('Motorola', 'C'); ?>, <?php echo $this->stencil->addLink('Zilog', 'C'); ?>, and National Semiconductor and to top the successful <?php echo $this->stencil->addLink('Z80', 'CPU'); ?>.</li>
+            <li><?php echo $this->stencil->addLink('186', 'CPU'); ?>: included a DMA controller, interrupt controller, timers, and chip select logic.</li>
+            <li><?php echo $this->stencil->addLink('286', 'CPU'); ?>: first x86 processor with protected mode</li>
+            <li><?php echo $this->stencil->addLink('i386', 'CPU'); ?>: first 32-bit x86 processor</li>
+            <li><?php echo $this->stencil->addLink('i486', 'CPU'); ?>: Intel's second generation of 32-bit x86 processors, introduced built-in floating point unit (FPU), 8KB on-chip L1 cache, and pipelining.</li>
+            <li><?php echo $this->stencil->addLink('P5', 'CPU'); ?>: original <?php echo $this->stencil->addLink('Pentium', 'CPU'); ?> microprocessors</li>
+            <li><?php echo $this->stencil->addLink('P6', 'CPU'); ?>: used in <?php echo $this->stencil->addLink('Pentium Pro', 'CPU'); ?>, <?php echo $this->stencil->addLink('Pentium II', 'CPU'); ?>, <?php echo $this->stencil->addLink('Pentium II Xeon', 'CPU'); ?>, <?php echo $this->stencil->addLink('Pentium III', 'CPU'); ?>, and <?php echo $this->stencil->addLink('Pentium III Xeon', 'CPU'); ?> microprocessors.
+                <ul>
+                    <li><?php echo $this->stencil->addLink('Pentium M', 'CPU'); ?>: updated version of Pentium III's P6 microarchitecture designed from the ground up for mobile computing.</li>
+                    <li><?php echo $this->stencil->addLink('Enhanced Pentium M', 'CPU'); ?>: updated, dual core version of the Pentium M microarchitecture used in <?php echo $this->stencil->addLink('Core', 'CPU'); ?> microprocessors.</li>
+                </ul>
+            </li>
+            <li><?php echo $this->stencil->addLink('NetBurst', 'CPU'); ?>: used in <?php echo $this->stencil->addLink('Pentium 4', 'CPU'); ?>, <?php echo $this->stencil->addLink('Pentium D', 'CPU'); ?>, and some <?php echo $this->stencil->addLink('Xeon', 'CPU'); ?> microprocessors. Commonly referred to as <strong>P7</strong> although its internal name was P68 (P7 was used for <?php echo $this->stencil->addLink('Itanium', 'CPU'); ?>). Later revisions were the first to feature Intel's x86-64 architecture.</li>
+            <li><?php echo $this->stencil->addLink('Core', 'CPU'); ?>: reengineered P6-based microarchitecture used in <?php echo $this->stencil->addLink('Core 2', 'CPU'); ?> and <?php echo $this->stencil->addLink('Xeon', 'CPU'); ?> microprocessors, built on a 65 nm process.
+                <ul>
+                    <li><strong>Penryn</strong>: 45 nm shrink of the Core microarchitecture with larger cache, higher FSB and clock speeds, and SSE4.1 instructions.</li>
+                </ul>
+            </li>
+            <li><?php echo $this->stencil->addLink('Nehalem', 'CPU'); ?>: released 17 November 2008, built on a 45 nm process and used in the <?php echo $this->stencil->addLink('Core i&', 'CPU'); ?>, <?php echo $this->stencil->addLink('Core i5', 'CPU'); ?>, <?php echo $this->stencil->addLink('Core i3', 'CPU'); ?> microprocessors. Incorporates the memory controller into the CPU die.
+                <ul>
+                    <li><strong>Westmere</strong>: 32 nm shrink of the Nehalem microarchitecture with several new features.</li>
+                </ul>
+            </li>
+            <li><?php echo $this->stencil->addLink('Sandy Bridge', 'CPU'); ?>: released 9 January 2011, built on a 32 nm process and used in the <?php echo $this->stencil->addLink('Core i7', 'CPU'); ?>, <?php echo $this->stencil->addLink('Core i5', 'CPU'); ?>, <?php echo $this->stencil->addLink('Core i3', 'CPU'); ?> second generation microprocessors, and in Pentium B9XX and <?php echo $this->stencil->addLink('Celeron', 'CPU'); ?> B8XX series. Formerly called <strong>Gesher</strong> but renamed in 2007.
+                <ul>
+                    <li><strong>Ivy Bridge</strong>: 22 nm shrink of the Sandy Bridge microarchitecture released 28 April 2012.</li>
+                </ul>
+            </li>
+            <li><?php echo $this->stencil->addLink('Haswell', 'CPU'); ?>: new 22 nm microarchitecture, released 3 June 2013.
+                <ul>
+                    <li><strong>Broadwell</strong>: 14 nm shrink of the Haswell microarchitecture, released in September 2014. Formerly called Rockwell.</li>
+                </ul>
+            </li>
+            <li><?php echo $this->stencil->addLink('Skylake', 'CPU'); ?>: future Intel microarchitecture, based on a 14 nm process.
+                <ul>
+                    <li><strong>Cannonlake</strong>: 10 nm shrink of the Skylake microarchitecture. Formerly called <strong>Skymont</strong>.</li>
+                </ul>
+            </li>
+            <li><?php echo $this->stencil->addLink('Bonnell', 'CPU'); ?>: 45 nm, low-power, in-order microarchitecture for use in <?php echo $this->stencil->addLink('Atom', 'CPU'); ?> processors.
+                <ul>
+                    <li><strong>Saltwell</strong>: 32 nm shrink of the Bonnell microarchitecture.</li>
+                </ul>
+            </li>
+            <li><?php echo $this->stencil->addLink('Silvermount', 'CPU'); ?>: 22 nm, out-of-order microarchitecture for use in <?php echo $this->stencil->addLink('Atom', 'CPU'); ?> processors.
+                <ul>
+                    <li><strong>Airmont</strong>: 14 nm shrink of the Silvermont microarchitecture.</li>
+                </ul>
+            </li>
+            <li><strong>Goldmont</strong>: 14 nm Atom microarchitecture.
+        </ul>
+    </div>
+</div>
