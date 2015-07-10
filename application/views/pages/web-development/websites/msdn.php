97ce733d1a649b1446b6d2f0dd2bf6944b87a91b
Iain White <iain@whiteinternet.com>
2015-07-10 16:52:24 +1000

    New Pages

---
+++ application/views/pages/web-development/websites/msdn.php
@@ -0,0 +1,6 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('MSDN - Microsoft Developer Network'); ?>
+        <p><strong>Microsoft Developer Network</strong>, widely referred to by its abbreviation, <strong>MSDN</strong>, is the portion of <?php echo $this->stencil->addLink('Microsoft', 'C'); ?> responsible for managing the firm's relationship with developers and testers, such as hardware developers interested in the operating system (OS), and software developers developing on the various OS platforms or using the API and/or scripting languages of Microsoft's applications. The relationship management is situated in assorted media: web sites, newsletters, developer conferences, trade media, blogs and DVD distribution. The life cycle of the relationships ranges from legacy support through evangelizing potential offerings.</p>
+    </div>
+</div>
