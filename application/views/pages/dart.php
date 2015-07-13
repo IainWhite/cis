9479dd9916f564d75e5892d837195f2cb899c888
Iain White <iain@whiteinternet.com>
2015-07-13 11:01:41 +1000

    New Pages

---
+++ application/views/pages/dart.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Dart Programming Language'); ?>
+        <p><strong>Dart</strong> is an open-source Web programming language developed by <?php echo $this->stencil->addLink('Google', 'C'); ?>. It was unveiled at the GOTO conference in Aarhus, Denmark, 10 - 12 October 2011. In order to run in mainstream browsers, Dart relies on a source-to-source compiler to <?php echo $this->stencil->addLink('JavaScript', 'L'); ?>. According to the project site, Dart was "<em>designed to be easy to write development tools for, well-suited to modern app development, and capable of high-performance implementations</em>".</p>
+        <p>Dart is a class-based, single-inheritance, object-oriented language with C-style syntax. It supports interfaces, abstract classes, reified generics, and optional typing. Static type annotations do not affect the runtime semantics of the code. Instead, the type annotations can provide documentation for tools like static checkers and dynamic runtime checks.</p>
+    </div>
+</div>
