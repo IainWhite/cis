8df2ca3dc883aa9307a3b8c9d76a2a927fc26cee
Iain White <iain@whiteinternet.com>
2015-07-17 10:15:39 +1000

    New Pages

---
+++ application/views/pages/web-development/xml/xsl.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('XSL - Extensible Stylesheet Language'); ?>
+        <p><strong>Extensible Stylesheet Language (XSL)</strong> is used to refer to a family of languages used to transform and render <?php echo $this->stencil->addLink('XML', 'WD'); ?>, documents.</p>
+        <p>Historically, the <strong>XSL Working Group</strong> in <?php echo $this->stencil->addLink('W3C', 'C'); ?>, produced a draft specification under the name XSL, which eventually split into three parts:</p>
+        <ul>
+            <li><strong>XSL Transformation</strong> (<?php echo $this->stencil->addLink('XSLT', 'XML'); ?>,): an XML language for transforming XML documents</li>
+            <li><strong>XSL Formatting Objects</strong> (<?php echo $this->stencil->addLink('XSL-FO', 'XML'); ?>,): an XML language for specifying the visual formatting of an XML document</li>
+            <li><strong>XML Path Language</strong> (<?php echo $this->stencil->addLink('XPath', 'XML'); ?>,): a non-XML language used by XSLT, and also available for use in non-XSLT contexts, for addressing the parts of an XML document.</li>
+        </ul>
+        <p>As a result, the term XSL is now used with a number of different meanings:</p>
+        <p>Sometimes it refers to XSLT: this usage is best avoided. However, "xsl" is used both as the conventional namespace prefix for the XSLT namespace, and as the conventional filename suffix for files containing XSLT stylesheet modules</p>
+        <p>Sometimes it refers to XSL-FO: this usage can be justified by the fact that the XSL-FO specification carries the title Extensible Stylesheet Language (XSL); however, the term XSL-FO is less likely to be misunderstood</p>
+        <p>Sometimes it refers to both languages considered together, or to the working group that develops both languages</p>
+        <p>Sometimes, especially in the <?php echo $this->stencil->addLink('Microsoft', 'C'); ?>, world, it refers to a now obsolete variant of XSLT developed and shipped by Microsoft as part of <?php echo $this->stencil->addLink('MSXML', 'XML'); ?>, before the W3C specification was finalised.</p>
+    </div>
+</div>
---
+++ application/views/pages/web-development/xml/xslt.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('XSLT (Extensible Stylesheet Language Transformations)'); ?>
+        <p><strong>XSLT (Extensible Stylesheet Language Transformations)</strong> is a language for transforming <?php echo $this->stencil->addLink('XML', 'WD'); ?> documents into other XML documents, or other formats such as <?php echo $this->stencil->addLink('HTMl', 'WD'); ?> for web pages, plain text or into <?php echo $this->stencil->addLink('XSL Formatting Objects', 'XML'); ?>, which may subsequently be converted to other formats, such as PDF PostScript and PNG.</p>
+        <p>The original document is not changed; rather, a new document is created based on the content of an existing one. Typically, input documents are XML files, but anything from which the processor can build an XQuery and XPath Data Model can be used, for example relational database tables, or geographical information systems.</p>
+        <p>XSLT is a Turing-complete language, meaning it can specify any computation that can be performed by a computer.</p>
+    </div>
+</div>
