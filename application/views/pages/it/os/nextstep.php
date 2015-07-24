d01868ff0cb0a0c903c00069cc7302d076e23740
Iain White <iain@whiteinternet.com>
2015-07-24 11:50:57 +1000

    New Pages OS X

---
+++ application/views/pages/it/os/nextstep.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('NeXTSTEP'); ?>
+        <p><strong>NeXTSTEP</strong> is an object-oriented, multitasking operating system which was developed by <?php echo $this->stencil->addLink('NeXT Inc', 'C'); ?> to run on its range of proprietary workstation computers, such as the <strong>NeXTcube</strong>. It was later ported to several other computer architectures.</p>
+        <p>NeXTSTEP was later modified to separate the underlying operating system from the higher-level object libraries.</p>
+        <p>The result was the <strong>OpenStep API</strong>, which ran on multiple underlying operating systems, including NeXT's own OPENSTEP.</p>
+        <p><?php echo $this->stencil->addLink('Apple', 'C'); ?>'s <?php echo $this->stencil->addLink('OS X', 'OS'); ?> and <?php echo $this->stencil->addLink('iOS', 'OS'); ?> are direct descendants of NeXTSTEP, through the OPENSTEP lineage.</p>
+    </div>
+</div>
---
+++ application/views/pages/it/os/os-x.php
@@ -0,0 +1,89 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('OS X'); ?>
+        <p><strong>OS X</strong> (previously <strong>Mac OS X</strong>) is a series of <?php echo $this->stencil->addLink('Unix-based', 'OS'); ?> graphical interface operating systems developed, marketed, and sold by <?php echo $this->stencil->addLink('Apple Inc', 'C'); ?>.</p>
+        <p>It is designed to run exclusively on <?php echo $this->stencil->addLink('Mac', 'ITH'); ?> computers, having been pre-loaded on all Macs since 2002. It was the successor to <?php echo $this->stencil->addLink('Mac OS 9', 'MOS'); ?>, released in 1999, the final release of the "classic" <?php echo $this->stencil->addLink('Mac OS', 'OS'); ?>, which had been Apple's primary operating system since 1984.</p>
+        <p>The first version released was Mac OS X Server 1.0 in 1999, and a desktop version, <?php echo $this->stencil->addLink('Mac OS X v10.0', 'OSX'); ?> "<em>Cheetah</em>" followed on 24 March 2001.</p>
+        <p>OS X, whose <em>X</em> is the Roman numeral for 10 and is a prominent part of its brand identity, is built on technologies developed at <?php echo $this->stencil->addLink('NeXT', 'C'); ?> between the second half of the 1980s and Apple's purchase of the company in late 1996. The '<em>X</em>' is also used to emphasize the relatedness between OS X and <?php echo $this->stencil->addLink('Unix', 'OS'); ?>.</p>
+        <p><?php echo $this->stencil->addLink('Mac OS X 10.7', 'OSX'); ?> "<em>Lion</em>" was the first version of OS X to drop support for <?php echo $this->stencil->addLink('32-bit', 'IT'); ?> <?php echo $this->stencil->addLink('Intel', 'C'); ?> processors and run exclusively on <?php echo $this->stencil->addLink('64-bit', 'IT'); ?> Intel CPUs.</p>
+    </div>
+</div>
+<?php if (!$this->stencil->getIsSub()): ?>
+    <div class="row">
+        <div class="col-lg-12 col-md-12">
+            <h3>Mac OS X Versions</h3>
+            <table class="table table-bordered table-hover table-striped">
+                <tr>
+                    <th>Version</th>
+                    <th>Codename</th>
+                    <th>Release date</th>
+                </tr>
+                <tr>
+                    <td>Mac OS X Server 1.0</td>
+                    <td>Hera</td>
+                    <td>March 1999</td>
+                </tr>
+                <tr>
+                    <td><?php echo $this->stencil->addLink('Mac OS X 10.0', 'OSX'); ?></td>
+                    <td><?php echo $this->stencil->addLink('Cheetah', 'OSX'); ?></td>
+                    <td>March 2001</td>
+                </tr>
+                <tr>
+                    <td><?php echo $this->stencil->addLink('Mac OS X 10.0.1', 'OSX'); ?></td>
+                    <td><?php echo $this->stencil->addLink('Puma', 'OSX'); ?></td>
+                    <td>September 2001</td>
+                </tr>
+                <tr>
+                    <td><?php echo $this->stencil->addLink('Mac OS X 10.0.2', 'OSX'); ?></td>
+                    <td><?php echo $this->stencil->addLink('Jaguar', 'OSX'); ?></td>
+                    <td>August 2002</td>
+                </tr>
+                <tr>
+                    <td><?php echo $this->stencil->addLink('Mac OS X 10.0.3', 'OSX'); ?></td>
+                    <td><?php echo $this->stencil->addLink('Panther', 'OSX'); ?></td>
+                    <td>October 2003</td>
+                </tr>
+                <tr>
+                    <td><?php echo $this->stencil->addLink('Mac OS X 10.0.4', 'OSX'); ?></td>
+                    <td><?php echo $this->stencil->addLink('Tiger', 'OSX'); ?></td>
+                    <td>April 2005</td>
+                </tr>
+                <tr>
+                    <td><?php echo $this->stencil->addLink('Mac OS X 10.0.5', 'OSX'); ?></td>
+                    <td><?php echo $this->stencil->addLink('Leopard', 'OSX'); ?></td>
+                    <td>October 2007</td>
+                </tr>
+                <tr>
+                    <td><?php echo $this->stencil->addLink('Mac OS X 10.0.6', 'OSX'); ?></td>
+                    <td><?php echo $this->stencil->addLink('Snow Leopard', 'OSX'); ?></td>
+                    <td>August 2009</td>
+                </tr>
+                <tr>
+                    <td><?php echo $this->stencil->addLink('Mac OS X 10.0.7', 'OSX'); ?></td>
+                    <td><?php echo $this->stencil->addLink('Lion', 'OSX'); ?></td>
+                    <td>July 2011</td>
+                </tr>
+                <tr>
+                    <td><?php echo $this->stencil->addLink('OS X 10.0.8', 'OSX'); ?></td>
+                    <td><?php echo $this->stencil->addLink('Mountain Lion', 'OSX'); ?></td>
+                    <td>July 2012</td>
+                </tr>
+                <tr>
+                    <td><?php echo $this->stencil->addLink('OS X 10.0.8', 'OSX'); ?></td>
+                    <td><?php echo $this->stencil->addLink('Mavericks', 'OSX'); ?></td>
+                    <td>October 2013</td>
+                </tr>
+                <tr>
+                    <td><?php echo $this->stencil->addLink('OS X 10.0.10', 'OSX'); ?></td>
+                    <td><?php echo $this->stencil->addLink('Yosemite', 'OSX'); ?></td>
+                    <td>October 2014</td>
+                </tr>
+                <tr>
+                    <td>OS X 10.9</td>
+                    <td>El Capitan</td>
+                    <td>2015</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+<?php endif; ?>
