6012994072e74e141bff467424a3454f84084232
Iain White <iain@whiteinternet.com>
2015-07-27 11:15:34 +1000

    Update

---
+++ application/views/pages/it/networks/vpn.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('VPN - Virtual Private Network'); ?>
+        <p>A <strong>Virtual Private Network</strong> (<strong>VPN</strong>) extends a private network across a public network, such as the <?php echo $this->stencil->addLink('Internet', 'ITN'); ?>.</p>
+        <p>It enables a computer to send and receive data across shared or public networks as if it is directly connected to the private network, while benefiting from the functionality, security and management policies of the private network.</p>
+        <p>A VPN is created by establishing a virtual point-to-point connection through the use of dedicated connections, virtual tunnelling protocols, or traffic encryptions.</p>
+        <p>Through VPN you can access your private network over Internet.</p>
+        <p>A virtual private network connection across the Internet is similar to a <strong>Wide Area Network (<?php echo $this->stencil->addLink('WAN', 'ITN'); ?>)</strong> link between sites.</p>
+        <p>From a user perspective, the extended network resources are accessed in the same way as resources available within the private network.</p>
+        <p>VPNs allow employees to securely access their company's internet while travelling outside the office.</p>
+        <p>Similarly, VPNs securely connect geographically disparate offices of an organization, creating one cohesive network.</p>
+        <p>VPN technology is also used by Internet users to connect to proxy servers for the purpose of protecting personal identity and location.</p>
+    </div>
+</div>
---
+++ application/views/pages/web-development/browser-wars.php
@@ -0,0 +1,99 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('Browser Wars'); ?>
+        <p><strong>Browser Wars</strong> is a metaphorical term that refers to competitions for dominance in usage share in the <?php echo $this->stencil->addLink('Web browser', 'WD'); ?> marketplace.</p>
+        <p>The term is often used to denote two specific rivalries: the competition that saw <?php echo $this->stencil->addLink('Microsoft', 'C'); ?>'s <?php echo $this->stencil->addLink('Internet Explorer', 'WD'); ?> replace <?php echo $this->stencil->addLink('Netscape', 'C'); ?>'s <?php echo $this->stencil->addLink('Navigator', 'WB'); ?> as the dominant browser during the late 1990s and the erosion of Internet Explorer's market share since 2003 by a collection of emerging browsers including <?php echo $this->stencil->addLink('Mozilla Firefox', 'WB'); ?>, <?php echo $this->stencil->addLink('Google Chrome', 'WB'); ?>, <?php echo $this->stencil->addLink('Safari', 'WB'); ?>, and <?php echo $this->stencil->addLink('Opera', 'WB'); ?>.</p>
+        <p>In more recent years, the introduction of <?php echo $this->stencil->addLink('HTML5', 'WD'); ?>, <?php echo $this->stencil->addLink('CSS-3', 'WD'); ?> and extensive client-side scripting to the <?php echo $this->stencil->addLink('World Wide Web', 'WD'); ?>, as well as more widespread use of <?php echo $this->stencil->addLink('smartphones', 'ITH'); ?> and other mobile devices for browsing the web, have added new dimensions and some new players, ensuring that boisterous browser battles continue on many fronts.</p>
+    </div>
+</div>
+
+<?php if (!$this->stencil->getIsSub()): ?>
+    <div class="row">
+        <div class="col-lg-12 col-md-12">
+            <h2>The First Browser War</h2>
+            <p>By mid-1995 the World Wide Web had received a great deal of attention in popular culture and the mass media.</p>
+            <p><?php echo $this->stencil->addLink('Netscape Navigator', 'WB'); ?> was the most widely used web browser and <?php echo $this->stencil->addLink('Microsoft', 'C'); ?> had licensed <?php echo $this->stencil->addLink('Mosaic', 'WB'); ?> to create <?php echo $this->stencil->addLink('Internet Explorer', 'WB'); ?>, which it had released as part of the <?php echo $this->stencil->addLink('Microsoft Windows 95', 'WIN'); ?> Plus! Pack in August.</p>
+            <p><?php echo $this->stencil->addLink('Internet Explorer 2', 'IE'); ?> was released as a free download three months later.</p>
+            <p>Unlike Netscape Navigator it was available to all <?php echo $this->stencil->addLink('Windows', 'OS'); ?> users for free, even commercial companies. Other companies later followed and gave their browsers away for free.</p>
+            <p>Both Netscape Navigator and competitor products like <strong>InternetWorks</strong>, <strong>Quarterdeck Browser</strong>, <strong>InterAp</strong>, and <strong>WinTapestry</strong> were bundled with other applications to full internet suites.</p>
+            <p>New versions of Internet Explorer and Netscape (branded as <?php echo $this->stencil->addLink('Netscape Communicator', 'WB'); ?>) were released at a rapid pace over the following few years.</p>
+            <p>Development was rapid and new features were routinely added, including Netscape's <?php echo $this->stencil->addLink('JavaScript', 'L'); ?> (subsequently replicated by Microsoft as <?php echo $this->stencil->addLink('JScript', 'JS'); ?>) and proprietary <?php echo $this->stencil->addLink('HTML', 'WD'); ?> tags such as <code>blink</code> and <code>marquee</code>.</p>
+            <p>Internet Explorer began to approach feature parity with Netscape with version 3.0 (1996), which offered scripting support and the market's first commercial Cascading Style Sheets (<?php echo $this->stencil->addLink('CSS', 'WD'); ?>) implementation.</p>
+            <p>In October 1997, <?php echo $this->stencil->addLink('Internet Explorer 4', 'IE'); ?> was released. The release party in San Francisco featured a ten-foot-tall letter "e" logo.</p>
+            <p><?php echo $this->stencil->addLink('Netscape', 'C'); ?> employees showing up to work the following morning found the giant logo on their front lawn, with a sign attached that read "<em>From the IE team ... We Love You</em>".</p>
+            <p>The Netscape employees promptly knocked it over and set a giant figure of their <?php echo $this->stencil->addLink('Mozilla', 'C'); ?> dinosaur mascot atop it, holding a sign reading "<em>Netscape 72, Microsoft 18</em>" representing the market distribution.</p>
+            <p>Internet Explorer 4 changed the tides of the browser wars.</p>
+            <p>It was integrated into <?php echo $this->stencil->addLink('Microsoft Windows', 'OS'); ?>, which some considered technologically disadvantageous and an apparent exploitation of Microsoft's monopoly on the PC platform.</p>
+            <p>Users were discouraged from using competing products because IE was "already there" on their PCs.</p>
+            <p>During these releases it was common for web designers to display '<em>best viewed in Netscape</em>' or '<em>best viewed in Internet Explorer</em>' logos.</p>
+            <p>These images often identified a specific browser version and were commonly linked to a source from which the stated browser could be downloaded.</p>
+            <p>These logos generally recognized the divergence between the standards supported by the browsers and signified which browser was used for testing the pages.</p>
+            <p>In response, supporters of the principle that web sites should be compliant with <strong>World Wide Web Consortium (<?php echo $this->stencil->addLink('W3C', 'C'); ?>)</strong> standards and hence viewable with any browser started the "<em>Viewable With Any Browser</em>" campaign, which employed its own logo similar to the partisan ones.</p>
+            <br />
+            <p>Microsoft had three strong advantages in the browser wars.</p>
+            <p>One was resources: Netscape began with about 80% market share and a good deal of public goodwill, but as a relatively small company deriving the great bulk of its income from what was essentially a single product (Navigator and its derivatives), it was financially vulnerable.</p>
+            <p>Netscape's total revenue never exceeded the interest income generated by Microsoft's cash on hand.</p>
+            <p>Microsoft's vast resources allowed them to make IE available without charge, as the enormous revenues from Windows were used to fund its development and marketing.</p>
+            <p>Netscape was commercial software for businesses but provided gratis for home and education users; Internet Explorer was provided gratis for all Windows and <?php echo $this->stencil->addLink('Macintosh', 'ITH'); ?> users, cutting off a significant revenue stream: As it was told by Jim Barksdale, President and CEO of Netscape Communications: "<em>Very few times in warfare have smaller forces overtaken bigger forces...</em>".</p>
+            <p>Another advantage was that Microsoft Windows had over 90% share of the desktop operating system market.</p>
+            <p>IE was bundled with every copy of Windows; therefore Microsoft was able to dominate the market share easily as customers had IE as a default.</p>
+            <p>In this time period, many new computer purchases were first computer purchases for home users or offices, and many of the users had never extensively used a web browser before, so had nothing to compare with and little motivation to consider alternatives; the great set of abilities they had gained with access to the Internet and the World Wide Web made any difference in browser features or ergonomics pale in comparison.</p>
+            <p>During the United States Microsoft antitrust case in 1998, <?php echo $this->stencil->addLink('Intel', 'C'); ?> vice president <strong>Steven McGeady</strong>, a witness called by the government, said on the stand that a senior executive at Microsoft told him in 1995 of his company's intention to "<em>cut off Netscape's air supply</em>".</p>
+            <p>A Microsoft attorney said that McGeady's testimony is not credible.</p>
+            <p>That same year, Netscape, the company, was acquired by <strong>America Online (<?php echo $this->stencil->addLink('AOL', 'C'); ?>)</strong> for US$4.2 billion.</p>
+            <p>Internet Explorer became the new dominant browser, attaining a peak of about 96% of the web browser usage share during 2002, more than Netscape had at its peak.</p>
+            <p>The first browser war ended with Internet Explorer having no remaining serious competition for its market share.</p>
+            <p>This also brought an end to the rapid innovation in web browsers; until 2006 there was only one new version of Internet Explorer since version 6.0 had been released in 2001.</p>
+            <p><?php echo $this->stencil->addLink('Internet Explorer 6', 'IE'); ?> Service Pack 1 was developed as part of <?php echo $this->stencil->addLink('Windows XP', 'WIN'); ?> SP1, and integrated into <?php echo $this->stencil->addLink('Windows Server 2003', 'WIS'); ?>.</p>
+            <p>Further enhancements were made to Internet Explorer in <?php echo $this->stencil->addLink('Windows XP', 'WIN'); ?> SP2 (released in 2004), including a pop-up blocker and stronger default security settings against the installation of <?php echo $this->stencil->addLink('ActiveX', 'IT'); ?> controls.</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12 col-md-12">
+            <h2>The Second Browser War</h2>
+            <p>After the defeat of <?php echo $this->stencil->addLink('Navigator', 'WB'); ?> by <?php echo $this->stencil->addLink('Internet Explorer', 'WB'); ?>, <?php echo $this->stencil->addLink('Netscape', 'C'); ?> open-sourced their browser code, and entrusted it to the newly formed non-profit <?php echo $this->stencil->addLink('Mozilla Foundation', 'C'); ?> a primarily community driven project to create a successor to Netscape.</p>
+            <p>Development continued for several years with little widespread adoption until a stripped-down browser-only version of the full suite was created, which included features, such as tabbed browsing and a separate search bar, both that had previously only appeared in <?php echo $this->stencil->addLink('Opera', 'WB'); ?>.</p>
+            <p>The browser-only version was initially named <strong>Phoenix</strong>, but because of trademark issues that name was changed, first to <strong>Firebird</strong>, then to <?php echo $this->stencil->addLink('Firefox', 'WB'); ?>.</p>
+            <p>This browser became the focus of the Mozilla Foundation's development efforts and <?php echo $this->stencil->addLink('Mozilla Firefox 1.0', 'WB'); ?> was released on 9 November 2004.</p>
+            <p>It then continued to gain an increasing share of the browser market until a peak in 2010, after which it has remained largely stable.</p>
+            <p>In 2003, Microsoft announced that <?php echo $this->stencil->addLink('Internet Explorer 6', 'IE'); ?> Service Pack 1 would be the last standalone version of its browser.</p>
+            <p>Future enhancements would be dependent on <?php echo $this->stencil->addLink('Windows Vista', 'WIN'); ?>, which would include new tools such as the WPF and XAML to enable developers to build extensive Web applications.</p>
+            <p>In response, in April 2004, the Mozilla Foundation and Opera Software joined efforts to develop new open technology standards which add more capability while remaining backward compatible with existing technologies.</p>
+            <p>The result of this collaboration was the WHATWG, a working group devoted to the fast creation of new standard definitions that would be submitted to the <?php echo $this->stencil->addLink('W3C', 'C'); ?> for approval.</p>
+            <p>On 15 February 2005, Microsoft announced that <?php echo $this->stencil->addLink('Internet Explorer 7', 'IE'); ?> would be available for <?php echo $this->stencil->addLink('Windows XP', 'WIN'); ?> SP2 and later versions of Windows by mid 2005.</p>
+            <p>The announcement introduced the new version of the browser as a major upgrade over Internet Explorer 6 SP1.</p>
+            <p><?php echo $this->stencil->addLink('Opera', 'WB'); ?> had been a long-time small player in the browser wars, known for introducing innovative features such as tabbed browsing and mouse gestures, as well as being lightweight but feature rich.</p>
+            <p>The software, however, was commercial, which hampered its adoption compared to its free rivals until 2005, when the browser became freeware.</p>
+            <p>On 20 June 2006, Opera Software released Opera 9 including an integrated source viewer, a BitTorrent client implementation and widgets.</p>
+            <p>It was the first Windows browser to pass the <strong>Acid2</strong> test.</p>
+            <p>Opera Mini, a mobile browser, has significant mobile market share as well as being available on the Nintendo DS and Wii.</p>
+            <p>Microsoft released <?php echo $this->stencil->addLink('Internet Explorer 7', 'IE'); ?> on 18 October 2006.</p>
+            <p>It included tabbed browsing, a search bar, a phishing filter, and improved support for Web standards - all features already familiar to Opera and Firefox users.</p>
+            <p>Microsoft distributed Internet Explorer 7 to genuine Windows users (WGA) as a high priority update through Windows Update.</p>
+            <p>Typical market share analysis showed only a slow uptake of Internet Explorer 7 and Microsoft decided to drop the requirement for WGA and made Internet Explorer 7 available to all Windows users in October 2007.</p>
+            <p>Throughout the two following years, Microsoft worked on <?php echo $this->stencil->addLink('Internet Explorer 8', 'IE'); ?>.</p>
+            <p>On 19 December 2007, the company announced that an internal build of that version had passed the Acid2 CSS test in "IE8 standards mode" - the last of the major browsers to do so.</p>
+            <p>Internet Explorer 8 was released on 19 March 2009.</p>
+            <p>New features included accelerators, improved privacy protection, a compatibility mode for pages designed for older browsers and improved support for various web standards.</p>
+            <p>It is the last version of Internet Explorer to be released for <?php echo $this->stencil->addLink('Windows XP', 'WIN'); ?>.</p>
+            <p>On 24 October 2006, <?php echo $this->stencil->addLink('Mozilla', 'C'); ?> released <?php echo $this->stencil->addLink('Mozilla Firefox 2.0', 'WB'); ?>.</p>
+            <p>It included the ability to reopen recently closed tabs, a session restore feature to resume work where it had been left after a crash, a phishing filter and a spell-checker for text fields.</p>
+            <p>Mozilla released <?php echo $this->stencil->addLink('Firefox 3.0', 'WB'); ?> on 17 June 2008, with performance improvements, and other new features. <?php echo $this->stencil->addLink('Firefox 3.5', 'WB'); ?> followed on 30 June 2009 with further performance improvements, native integration of audio and video, and more privacy features.</p>
+            <p><?php echo $this->stencil->addLink('Apple', 'C'); ?> had created forks of the open-source <?php echo $this->stencil->addLink('KHTML', 'web-development/layout-engines'); ?> and <?php echo $this->stencil->addLink('KJS', 'web-development-javascript-engines'); ?> layout  and <?php echo $this->stencil->addLink('JavaScript engines', 'WD'); ?> from the <?php echo $this->stencil->addLink('KDE', 'V'); ?> <?php echo $this->stencil->addLink('Konqueror', 'WB'); ?> browser in 2002.</p>
+            <p>They explained that those provided a basis for easier development than other technologies by virtue of being small (fewer than 140,000 lines of code), cleanly designed, and standards compliant.</p>
+            <p>The resulting layout engine became known as <?php echo $this->stencil->addLink('Webkit', 'web-development/layout-engine'); ?> and it was incorporated into the <?php echo $this->stencil->addLink('Safari', 'WB'); ?> browser that first shipped with <?php echo $this->stencil->addLink('Mac OS X v10.3', 'MOS'); ?>.</p>
+            <p>On 13 June 2003, Microsoft said it was discontinuing IE on the Mac platform and on 6 June 2007, Apple released a beta version of Safari for Microsoft Windows.</p>
+            <p>On 28 December 2007, Netscape announced that support for its Mozilla derived <?php echo $this->stencil->addLink('Netscape Navigator', 'WB'); ?> would be discontinued on 1 February 2008, suggesting its users migrate to Mozilla Firefox.</p>
+            <p>However, on 28 January 2008, Netscape announced that support would be extended to 1 March 2008, and mentioned <?php echo $this->stencil->addLink('Flock', 'WB'); ?> alongside <?php echo $this->stencil->addLink('Firefox', 'WB'); ?> as alternatives to its users.</p>
+            <p><?php echo $this->stencil->addLink('Google', 'C'); ?> released the <?php echo $this->stencil->addLink('Chrome', 'WB'); ?> browser for Microsoft Windows on 11 December 2008, using the same <?php echo $this->stencil->addLink('Webkit', 'web-development/layout-engines'); ?> rendering engine as <?php echo $this->stencil->addLink('Safari', 'WB'); ?> and a faster <?php echo $this->stencil->addLink('JavaScript engine', 'WD'); ?> called <?php echo $this->stencil->addLink('V8', 'web-development/javasctipt-engine'); ?>.</p>
+            <p>An open sourced version for the <?php echo $this->stencil->addLink('Windows', 'OS'); ?>, <?php echo $this->stencil->addLink('Mac OS X', 'OS'); ?> and <?php echo $this->stencil->addLink('Linux', 'OS'); ?> platforms was released under the name <?php echo $this->stencil->addLink('Chromium', 'WB'); ?>.</p>
+            <p>According to Net Applications, Chrome had gained a 3.6% usage share by October 2009.</p>
+            <p>During December 2009 and January 2010, StatCounter reported that its statistics indicated that Firefox 3.5 was the most popular browser, when counting individual browser versions, passing Internet Explorer 7 and 8 by a small margin.</p>
+            <p>This was the first time a global statistic has reported that a non-Internet Explorer browser version had exceeded the top Internet Explorer version in usage share since the fall of Netscape Navigator.</p>
+            <p>This feat, which GeekSmack called the "<em>dethron[ing of] Microsoft and its Internet Explorer 7 browser</em>", could largely be attributed to the fact that it came at a time when IE8 was replacing IE7 as the dominant Internet Explorer version.</p>
+            <p>No more than two months later IE 8 had established itself as the most popular browser version, a position which it still held as of March 2011.</p>
+            <p>It should also be noted that other major statistics, such as Net Applications, never reported any non-IE browser version as having a higher usage share than the most popular Internet Explorer version, although Firefox 3.5 was reported as the third most popular browser version between December 2009 and February 2010, to be replaced by Firefox 3.6 since April 2010, each ahead of IE7 and behind IE6 and IE8.</p>
+        </div>
+    </div>
+<?php endif; ?>
