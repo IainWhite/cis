ba72b7ac825bb4c1876cb7282fc80cff84ce2eb2
Iain White <iain@whiteinternet.com>
2015-07-30 16:12:28 +1000

    New Pages

---
+++ application/views/pages/it/hardware/ipod-touch.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <?php echo $this->stencil->setHeadline('iPod Touch'); ?>
+        <p>The <strong>iPod Touch</strong> is an <?php echo $this->stencil->addLink('iOS', 'OS'); ?> based all purpose pocket computer designed and marketed by <?php echo $this->stencil->addLink('Apple Inc', 'C'); ?> with a user interface that is touchscreen based. It can be used as a music and video player, digital camera, handheld game device, and personal digital assistant (PDA). It connects to the Internet only through <?php echo $this->stencil->addLink('Wi-Fi', 'ITN'); ?> base stations, does not use cellular data, and is therefore not a smartphone, though it has a similar design to the <?php echo $this->stencil->addLink('iPhone', 'ITH'); ?> and is often referred to as the "<em>phone without a phone</em>". As of May 2013, 100 million iPod Touch units have been sold.</p>
+        <p>All models of the same generation typically consist of identical features, processors, and performance, in addition to available operating system upgrades, differing only in outer color and internal storage space. An exception was the fifth generation, as the first version of the low-end (16GB) model was sold without a rear facing iSight photographic camera and lacked the iPod Touch loop. However, on 26 June 2014, the 16GB model was replaced by an upgraded model which includes the rear facing camera and complete colour options.</p>
+    </div>
+</div>
