3ba71f141f9f46b07168ced8a1773eff23d6d8e4
Iain White <iain@whiteinternet.com>
2016-02-11 06:47:53 +1000

    Local pass

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = '127.0.0.1';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'GoldCoin10';
+$db['default']['password'] = 'LocalPassword';
 $db['default']['database'] = 'iainw_ci';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
